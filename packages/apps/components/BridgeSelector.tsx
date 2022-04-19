9eea3ff5c53d5883be4ceebc31be9e1671d4799a
JayJay1024 <lygm1062@gmail.com>
2022-04-19 19:19:45 +0800

    chore: format apps code

--- packages/apps/components/BridgeSelector.tsx
+++ packages/apps/components/BridgeSelector.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image';
 
 const TokenOnChain = ({ token, chain }: { token: string; chain: string }) => (
   <div className="relative w-14 h-14">
-    <Image src={token} alt='...' layout='fill' />
+    <Image src={token} alt="..." layout="fill" />
     <span className="w-7 h-7 absolute top-auto bottom-1 left-auto -right-3">
-      <Image  src={chain} alt='...' layout="fill" />
+      <Image src={chain} alt="..." layout="fill" />
     </span>
   </div>
-)
+);
 
 export function BridgeSelector() {
   return (
     <div className="dark:bg-antDark p-5 w-auto">
-      <TokenOnChain token='/image/ring.svg' chain='/image/darwinia.png' />
-      <TokenOnChain token='/image/ring.svg' chain='/image/ethereum.png' />
+      <TokenOnChain token="/image/ring.svg" chain="/image/darwinia.png" />
+      <TokenOnChain token="/image/ring.svg" chain="/image/ethereum.png" />
     </div>
-  )
+  );
 }
--- packages/apps/components/TransferInput.tsx
+++ packages/apps/components/TransferInput.tsx
@@ -3,43 +3,34 @@ import { Form, Input, Button, Checkbox } from 'antd';
 export function TransferInput() {
   return (
     <div className="dark:bg-antDark p-5">
-      <Form
-        layout="vertical"
-      >
-        <Form.Item
-          label='Your send'
-          name='send'
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
+      <Form layout="vertical">
+        <Form.Item label="Your send" name="send" rules={[{ required: true, message: 'Please input your username!' }]}>
           <Input />
         </Form.Item>
         <Form.Item
-          label='Your receive'
-          name='receive'
+          label="Your receive"
+          name="receive"
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Sender"
-          name='sender'
+          name="sender"
           tooltip="Select an address sending the transaction."
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
           <Input />
         </Form.Item>
         <Form.Item
-          label='Receiver'
-          name='receiver'
+          label="Receiver"
+          name="receiver"
           tooltip="Please do not fill in the exchange account. After the transaction is confirmed, the account cannot be changed."
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label='Info'
-          name='info'
-        >
+        <Form.Item label="Info" name="info">
           <Input />
         </Form.Item>
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
@@ -49,5 +40,5 @@ export function TransferInput() {
         </Form.Item>
       </Form>
     </div>
-  )
+  );
 }
