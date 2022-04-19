ac0def7b2b1be4b45ba245506a2536889d96e2a3
JayJay1024 <lygm1062@gmail.com>
2022-04-19 19:17:52 +0800

    feat: token on chain component

--- packages/apps/components/BridgeSelector.tsx
+++ packages/apps/components/BridgeSelector.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
+const TokenOnChain = ({ token, chain }: { token: string; chain: string }) => (
+  <div className="relative w-14 h-14">
+    <Image src={token} alt='...' layout='fill' />
+    <span className="w-7 h-7 absolute top-auto bottom-1 left-auto -right-3">
+      <Image  src={chain} alt='...' layout="fill" />
+    </span>
+  </div>
+)
+
 export function BridgeSelector() {
   return (
     <div className="dark:bg-antDark p-5 w-auto">
-      <p>sssss</p>
+      <TokenOnChain token='/image/ring.svg' chain='/image/darwinia.png' />
+      <TokenOnChain token='/image/ring.svg' chain='/image/ethereum.png' />
     </div>
   )
 }
