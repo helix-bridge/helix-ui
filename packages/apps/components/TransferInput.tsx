92781e62f1555ed8912fae794f02566c0a274518
JayJay1024 <lygm1062@gmail.com>
2022-04-20 16:24:38 +0800

    chore(apps): package address item

--- packages/apps/components/TransferInput.tsx
+++ packages/apps/components/TransferInput.tsx
@@ -1,4 +1,5 @@
 import { Form, Input, Button, Typography, Select } from 'antd';
+import { LabelTooltipType } from 'antd/lib/form/FormItemLabel';
 import Image from 'next/image';
 import styles from './TransferInput.module.scss';
 import classNames from 'classnames/bind';
@@ -35,39 +36,36 @@ const AmountItem = ({ label, name }: { label: string; name: string; }) => (
   </Form.Item>
 );
 
+const AddressItem = ({ label, name, tooltip }: { label: string, name: string, tooltip: LabelTooltipType }) => (
+  <Form.Item
+    label={label}
+    name={name}
+    tooltip={tooltip}
+    rules={[{ required: true, message: 'Please input your username!' }]}
+  >
+    <Select defaultValue="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1" size="large">
+      <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1">
+        <Typography.Text ellipsis className="w-4/5">
+          5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1
+        </Typography.Text>
+      </Select.Option>
+      <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2">
+        <Typography.Text ellipsis className="w-4/5">
+          5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2
+        </Typography.Text>
+      </Select.Option>
+    </Select>
+  </Form.Item>
+);
+
 export function TransferInput() {
   return (
     <div className="dark:bg-antDark p-5">
       <Form layout="vertical">
         <AmountItem label="Your send" name="send" />
         <AmountItem label="Your receive" name="receive" />
-        <Form.Item
-          label="Sender"
-          name="sender"
-          tooltip="Select an address sending the transaction."
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Select defaultValue="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1" size="large">
-            <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1">
-              <Typography.Text ellipsis className="w-4/5">
-                5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1
-              </Typography.Text>
-            </Select.Option>
-            <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2">
-              <Typography.Text ellipsis className="w-4/5">
-                5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2
-              </Typography.Text>
-            </Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Receiver"
-          name="receiver"
-          tooltip="Please do not fill in the exchange account. After the transaction is confirmed, the account cannot be changed."
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input size="large" />
-        </Form.Item>
+        <AddressItem label='Sender' name='sender' tooltip="Select an address sending the transaction."  />
+        <AddressItem label='Receiver' name='receiver' tooltip="Please do not fill in the exchange account. After the transaction is confirmed, the account cannot be changed."  />
         <Form.Item label="Info" name="info" className="relative">
           <Input disabled className="h-20" />
           <div className="absolute top-0 left-0 h-20 w-full flex flex-col justify-center space-y-2 px-4">
