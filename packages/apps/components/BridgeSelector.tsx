3e970ef95c6c606279b4e716e8064d816bac0c15
JayJay1024 <lygm1062@gmail.com>
2022-04-20 15:51:51 +0800

    chore(apps): transfer input token selector

--- packages/apps/components/BridgeSelector.tsx
+++ packages/apps/components/BridgeSelector.tsx
@@ -11,7 +11,7 @@ type TokenOnChainData = {
 };
 
 const TokenOnChain = ({ tokenIcon, chainIcon, chainName, from }: TokenOnChainData) => (
-  <div className='flex items-center'>
+  <div className="flex items-center">
     <div className={`relative w-14 h-14 ${from ? 'order-1' : 'order-2 ml-3'}`}>
       <Image src={tokenIcon} alt="..." layout="fill" />
       <span className="w-7 h-7 absolute top-auto bottom-1 left-auto -right-3">
@@ -25,24 +25,24 @@ const TokenOnChain = ({ tokenIcon, chainIcon, chainName, from }: TokenOnChainDat
   </div>
 );
 
-const SelectorItem = ({ value, theSelected }: { value: number, theSelected: number }) => {
+const SelectorItem = ({ value, theSelected }: { value: number; theSelected: number }) => {
   return (
-    <Radio.Button className='w-full' style={{ height: 'fit-content' }} value={value}>
+    <Radio.Button className="w-full" style={{ height: 'fit-content' }} value={value}>
       <div className="relative flex justify-between items-center pr-3">
-        {value === theSelected && <CheckSquareFilled className="absolute -top-px left-auto -right-4"/>}
-        <TokenOnChain tokenIcon="/image/ring.svg" chainIcon="/image/darwinia.png" chainName='Darwinia' from />
+        {value === theSelected && <CheckSquareFilled className="absolute -top-px left-auto -right-4" />}
+        <TokenOnChain tokenIcon="/image/ring.svg" chainIcon="/image/darwinia.png" chainName="Darwinia" from />
         <div className="relative w-56 flex justify-center">
           <div className="py-1 w-24 rounded-3xl bg-gray-700 flex justify-center items-center space-x-2 z-10">
-            <Image alt='...' src='/image/helix-bridge.svg' width={28} height={28} />
+            <Image alt="..." src="/image/helix-bridge.svg" width={28} height={28} />
             <strong>Helix</strong>
           </div>
-          <Image alt='...' src='/image/bridge-to.svg' layout="fill" />
+          <Image alt="..." src="/image/bridge-to.svg" layout="fill" />
         </div>
-        <TokenOnChain tokenIcon="/image/ring.svg" chainIcon="/image/ethereum.png" chainName='Ethereum' />
+        <TokenOnChain tokenIcon="/image/ring.svg" chainIcon="/image/ethereum.png" chainName="Ethereum" />
       </div>
     </Radio.Button>
-  )
-}
+  );
+};
 
 export function BridgeSelector() {
   const [theSelected, setTheSelected] = useState<number>(0);
@@ -56,15 +56,17 @@ export function BridgeSelector() {
         />
       ) : (
         <Radio.Group
-        className='w-full'
-        size='large'
-        defaultValue={theSelected}
-        onChange={(e) => setTheSelected(e.target.value)}
-      >
-        <Space direction="vertical" className='w-full' size="middle">
-          {(new Array(10)).fill(1).map((_, index) => <SelectorItem key={index} value={index} theSelected={theSelected} />)}
-        </Space>
-      </Radio.Group>
+          className="w-full"
+          size="large"
+          defaultValue={theSelected}
+          onChange={(e) => setTheSelected(e.target.value)}
+        >
+          <Space direction="vertical" className="w-full" size="middle">
+            {new Array(10).fill(1).map((_, index) => (
+              <SelectorItem key={index} value={index} theSelected={theSelected} />
+            ))}
+          </Space>
+        </Radio.Group>
       )}
     </div>
   );
--- packages/apps/components/TransferInput.tsx
+++ packages/apps/components/TransferInput.tsx
@@ -1,18 +1,46 @@
 import { Form, Input, Button, Typography, Select } from 'antd';
+import Image from 'next/image';
+import styles from './TransferInput.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const TokenSelector = ({ defaultValue }: { defaultValue: number }) => (
+  <Select defaultValue={defaultValue} className={cx('token-select')}>
+    {new Array(8).fill(1).map((_, index) => (
+      <Select.Option key={index} value={index}>
+        <div className={cx('flex items-center space-x-2 py-2')}>
+          <Image src="/image/ring.svg" alt="..." width={40} height={40} />
+          <div className="flex flex-col space-y-0">
+            <strong className="font-medium text-sm">RING</strong>
+            <small className="font-light text-sm">Darwinia</small>
+          </div>
+        </div>
+      </Select.Option>
+    ))}
+  </Select>
+);
 
 export function TransferInput() {
   return (
     <div className="dark:bg-antDark p-5">
       <Form layout="vertical">
         <Form.Item label="Your send" name="send" rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input size='large' />
+          <Input size="large" className='h-20' />
+          <div className="absolute top-0 left-auto right-0 h-20 flex justify-center items-center px-3">
+            <TokenSelector defaultValue={1} />
+          </div>
         </Form.Item>
         <Form.Item
           label="Your receive"
           name="receive"
           rules={[{ required: true, message: 'Please input your username!' }]}
+          className="relative"
         >
-          <Input size='large' />
+          <Input size="large" className='h-20' />
+          <div className="absolute top-0 left-auto right-0 h-20 flex justify-center items-center px-3">
+            <TokenSelector defaultValue={1} />
+          </div>
         </Form.Item>
         <Form.Item
           label="Sender"
@@ -20,12 +48,16 @@ export function TransferInput() {
           tooltip="Select an address sending the transaction."
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Select defaultValue='5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1' size="large">
-            <Select.Option value='5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1'>
-              <Typography.Text ellipsis className="w-4/5">5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1</Typography.Text>
+          <Select defaultValue="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1" size="large">
+            <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1">
+              <Typography.Text ellipsis className="w-4/5">
+                5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat1
+              </Typography.Text>
             </Select.Option>
-            <Select.Option value='5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2'>
-              <Typography.Text ellipsis className="w-4/5">5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2</Typography.Text>
+            <Select.Option value="5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2">
+              <Typography.Text ellipsis className="w-4/5">
+                5G1gFmXNy9rwYqryzTkyM4x5BSTB9cG4fYgqPz1SZ9VPMnat2
+              </Typography.Text>
             </Select.Option>
           </Select>
         </Form.Item>
@@ -35,23 +67,23 @@ export function TransferInput() {
           tooltip="Please do not fill in the exchange account. After the transaction is confirmed, the account cannot be changed."
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Input size='large' />
+          <Input size="large" />
         </Form.Item>
         <Form.Item label="Info" name="info" className="relative">
           <Input disabled className="h-20" />
-          <div className='absolute top-0 left-0 h-20 w-full flex flex-col justify-center space-y-2 px-4'>
-            <div className='flex justify-between items-center'>
+          <div className="absolute top-0 left-0 h-20 w-full flex flex-col justify-center space-y-2 px-4">
+            <div className="flex justify-between items-center">
               <Typography.Text>Bridge Name</Typography.Text>
               <Typography.Text>Helix Bridge</Typography.Text>
             </div>
-            <div className='flex justify-between items-center'>
+            <div className="flex justify-between items-center">
               <Typography.Text>Transaction Fee</Typography.Text>
               <Typography.Text>50 RING</Typography.Text>
             </div>
           </div>
         </Form.Item>
         <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
-          <Button type="primary" htmlType="submit" className='w-full' size='large'>
+          <Button type="primary" htmlType="submit" className="w-full" size="large">
             Transfer
           </Button>
         </Form.Item>
