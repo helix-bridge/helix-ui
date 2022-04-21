b4c40e5e74099f32356778d8b9e844889e10cf2f
JayJay1024 <lygm1062@gmail.com>
2022-04-21 17:23:39 +0800

    feat(apps): base modal

---
+++ packages/apps/components/BaseModal.tsx
@@ -0,0 +1,22 @@
+import { Modal, ModalProps } from 'antd';
+import { PropsWithChildren, memo } from 'react';
+import classNames from 'classnames/bind';
+import styles from './BaseModal.module.scss';
+import Image from 'next/image';
+
+const cx = classNames.bind(styles);
+
+const Component = ({
+  children,
+  ...others
+}:  PropsWithChildren<ModalProps>) => (
+  <Modal
+    { ...others }
+    className={cx('main')}
+    closeIcon={<Image alt='...' src='/image/modal-close.svg' width={16} height={16} />}
+  >
+    {children}
+  </Modal>
+);
+
+export const BaseModal = memo(Component);
