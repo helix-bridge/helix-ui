df46fab22f564dbbbc6ef9724398e4bf7008bfe8
JayJay1024 <lygm1062@gmail.com>
2022-04-21 23:46:12 +0800

    feat: select a token modal

---
+++ packages/apps/components/SelectTokenModal.tsx
@@ -0,0 +1,36 @@
+import { BaseModal } from './BaseModal';
+import { Button, Input, Tag, Typography } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { memo } from 'react';
+import Image from 'next/image';
+
+const Component = ({ visible, onSelect, onCancel }: { visible: boolean; onCancel: () => void; onSelect: (value: unknown) => void }) => (
+  <BaseModal
+    title='Select a token'
+    visible={visible}
+    footer={null}
+    width={540}
+    onCancel={onCancel}
+  >
+    <Input suffix={<SearchOutlined />} size='large' placeholder='Search Symbol or Paste Contract Address' />
+    <div className='mt-1'>
+      {(new Array(9)).fill(1).map((_, index) => <Button key={index} className='mt-2 mr-2' size='small'>Chain</Button>)}
+    </div>
+    <div className='max-h-96 overflow-auto mt-5'>
+      <div className='flex flex-col space-y-2'>
+        {(new Array(9)).fill(1).map((_, index) => (
+          <div className='flex items-center justify-between border border-gray-800 bg-gray-900 py-3 px-2' key={index} onClick={() => onSelect(index)}>
+            <div className='flex items-center space-x-2'>
+              <Image alt='...' src='/image/usdt.svg' width={36} height={36} />
+              <Typography.Text>USDT</Typography.Text>
+              <Tag color='processing'>Ethereum</Tag>
+            </div>
+            <Typography.Text>3,243.4387</Typography.Text>
+          </div>
+        ))}
+      </div>
+    </div>
+  </BaseModal>
+);
+
+export const SelectTokenModal = memo(Component);
