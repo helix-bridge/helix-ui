5d74ea06d806705cf1575b6ce4a953aea0866174
sxlwar <sxlwar@163.com>
2022-06-14 12:26:35 +0800

    chore: bump test chain spec version

--- packages/shared/config/network/pangoro.ts
+++ packages/shared/config/network/pangoro.ts
@@ -15,7 +15,7 @@ export const pangoroConfig: PolkadotChainConfig = {
     twitter: 'https://twitter.com/DarwiniaNetwork',
   },
   ss58Prefix: 18,
-  specVersion: 28110,
+  specVersion: 28120,
   tokens: [
     {
       name: 'ORING',
