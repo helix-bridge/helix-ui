86c81dcde6b69ee7d4696de4fc7b55b85e0e8b9a
sxlwar <sxlwar@163.com>
2022-06-30 16:38:57 +0800

    test: update apps history item

--- packages/apps/__tests__/components/HistoryItem.spec.tsx
+++ packages/apps/__tests__/components/HistoryItem.spec.tsx
@@ -2,6 +2,9 @@
 
 import { HistoryItem } from '../../components/record/HistoryItem';
 import { create } from 'react-test-renderer';
+import { Button } from 'antd';
+
+jest.useFakeTimers('modern').setSystemTime(new Date('2022-06-17'));
 
 describe('<HistoryItem />', () => {
   it('render history item', () => {
@@ -11,6 +14,22 @@ describe('<HistoryItem />', () => {
         token={{ amount: '8', symbol: 'RING', logo: 'ring.svg' }}
         process={<span>Just for test</span>}
       />
+    );
+
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('render history item with children', () => {
+    const component = create(
+      <HistoryItem
+        record={{ result: 1, startTime: 1652689116 }}
+        token={{ amount: '8', symbol: 'RING', logo: 'ring.svg' }}
+        process={<span>Just for test</span>}
+      >
+        <Button>Claim</Button>
+      </HistoryItem>
     );
 
     let tree = component.toJSON();
