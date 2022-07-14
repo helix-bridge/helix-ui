4366f0ec01aa0cf9738bb023b2bf7d38feb3a338
sxlwar <sxlwar@163.com>
2022-07-14 13:51:50 +0800

    chore: forbidden parachain to crab (#105)
    * chore: forbidden bridge selection instead of token selection

--- packages/apps/components/form-control/BridgeSelector.tsx
+++ packages/apps/components/form-control/BridgeSelector.tsx
@@ -3,7 +3,7 @@ import { Badge, Radio, Result, Space, Tooltip } from 'antd';
 import { matches } from 'lodash';
 import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useEffect } from 'react';
 import { Logo } from 'shared/components/widget/Logo';
 import { DEFAULT_DIRECTION } from 'shared/config/constant';
@@ -47,11 +47,24 @@ export function BridgeSelector({ direction, value, onChange }: BridgeSelectorPro
   const origin = { from: { name: from.name, type: from.type }, to: { name: to.name, type: to.type } };
   const isDefault = matches(origin);
 
+  const isDisabled = useCallback<(bridge: Bridge) => boolean>(
+    // eslint-disable-next-line complexity
+    (bridge: Bridge) => {
+      return (
+        !!direction.from &&
+        !!direction.to &&
+        ((bridge.disableIssuing && bridge.isIssuing(direction.from.meta, direction.to.meta)) ||
+          (bridge.disableRedeem && bridge.isRedeem(direction.from.meta, direction.to.meta)))
+      );
+    },
+    [direction.from, direction.to]
+  );
+
   useEffect(() => {
-    if (!value && bridges.length && onChange) {
+    if (!value && bridges.length && onChange && !isDisabled(bridges[0])) {
       onChange(bridges[0]);
     }
-  }, [value, bridges, onChange]);
+  }, [bridges, isDisabled, onChange, value]);
 
   return (
     <div className="p-5 overflow-auto" style={{ maxHeight: '65vh', minHeight: '20vh' }}>
@@ -103,6 +116,7 @@ export function BridgeSelector({ direction, value, onChange }: BridgeSelectorPro
                   className="w-full bg-gray-900 relative"
                   style={{ height: 'fit-content' }}
                   value={item}
+                  disabled={isDisabled(item)}
                 >
                   <div className={`relative flex justify-between items-center pr-3 py-3 ${needClaim ? 'pt-8' : ''}`}>
                     <TokenOnChain token={direction.from as CrossToken} isFrom />
