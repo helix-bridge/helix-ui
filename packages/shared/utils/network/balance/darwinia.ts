2b75f1484fb8619db86f846f685bcc555aabc1ef
sxlwar <sxlwar@163.com>
2022-08-08 11:01:26 +0800

    fix: use available balance instead of total balance

--- packages/shared/utils/network/balance/darwinia.ts
+++ packages/shared/utils/network/balance/darwinia.ts
@@ -1,45 +1,61 @@
-import { AccountData, AccountInfo } from '@darwinia/types';
-import { BN_ZERO } from '@polkadot/util';
-import BN from 'bn.js';
+import { AccountInfo } from '@darwinia/types';
+import '@polkadot/api-augment';
+import { PalletBalancesBalanceLock } from '@polkadot/types/lookup';
+import { BN, bnMax, BN_ZERO } from '@polkadot/util';
 import { entrance, waitUntilConnected } from '../../connection';
+
+// eslint-disable-next-line complexity, @typescript-eslint/no-explicit-any
+const calcMax = (lockItem: any, current: BN) => {
+  let max = current;
+
+  if (lockItem.reasons && !lockItem.reasons.isFee) {
+    max = bnMax(lockItem.amount, max);
+  } else if (lockItem.lockReasons && !lockItem.lockReasons.isFee) {
+    if (lockItem.lockFor.isCommon) {
+      max = bnMax(lockItem.lockFor.asCommon.amount, max);
+    } else if (lockItem.lockFor.isStaking) {
+      max = bnMax(lockItem.lockFor.asStaking.stakingAmount, max);
+    }
+  }
+
+  return max;
+};
 
 /**
  * @description other api can get balances:  api.derive.balances.all, api.query.system.account;
  * @see https://github.com/darwinia-network/wormhole-ui/issues/142
  */
+// eslint-disable-next-line complexity
 export async function getBalance(provider: string, account: string): Promise<[BN, BN]> {
   const api = entrance.polkadot.getInstance(provider);
 
   await waitUntilConnected(api);
 
   try {
-    // type = 0 query ring balance.  type = 1 query kton balance.
-    /* eslint-disable */
-    const ringUsableBalance = await (api.rpc as any).balances.usableBalance(0, account);
-    const ktonUsableBalance = await (api.rpc as any).balances.usableBalance(1, account);
-    /* eslint-enable */
+    const {
+      data: { free, freeKton },
+    } = await api.query.system.account<AccountInfo>(account);
 
-    return [new BN(ringUsableBalance.usableBalance.toString()), new BN(ktonUsableBalance.usableBalance.toString())];
-  } catch (error: unknown) {
-    console.warn(
-      '%c [ Failed to  querying balance through rpc ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      (error as Record<string, string>).message
-    );
-  }
+    const locks = await api.query.balances.locks<PalletBalancesBalanceLock[]>(account);
+    const ktonLocks = (await api.query.kton?.locks<PalletBalancesBalanceLock[]>(account)) ?? [];
 
-  try {
-    const { data } = (await api.query.system.account(account)) as AccountInfo;
-    const { free, freeKton } = data as unknown as AccountData;
+    let maxLock = BN_ZERO;
+    let maxKtonLock = BN_ZERO;
 
-    return [new BN(free.toString()), new BN(freeKton.toString())];
-  } catch (error) {
-    console.warn(
-      '%c [ Failed to  querying balance through account info ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      (error as Record<string, string>).message
-    );
+    locks.forEach((item) => {
+      maxLock = calcMax(item, maxLock);
+    });
 
+    ktonLocks.forEach((item) => {
+      maxKtonLock = calcMax(item, maxKtonLock);
+    });
+
+    const ring = free.sub(maxLock);
+    const kton = freeKton?.sub(maxKtonLock) ?? BN_ZERO;
+
+    return [ring.isNeg() ? BN_ZERO : ring, kton.isNeg() ? BN_ZERO : kton];
+  } catch (err) {
+    console.error(err);
     return [BN_ZERO, BN_ZERO];
   }
 }
