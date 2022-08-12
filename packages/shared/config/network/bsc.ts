4e6eeec9e707fddad542a10c4225823c10a5367d
sxlwar <sxlwar@163.com>
2022-08-12 15:55:36 +0800

    chore: rename bsc display name

--- packages/shared/config/network/bsc.ts
+++ packages/shared/config/network/bsc.ts
@@ -12,7 +12,7 @@ export const bscConfig: EthereumChainConfig = {
   isTest: false,
   logos: [{ name: 'bsc.png', type: 'main' }],
   name: 'bsc',
-  fullName: 'Binance Smart Chain',
+  fullName: 'BNB Chain',
   // provider: 'wss://dex.binance.org/api/ws',
   provider: 'https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3',
   social: {
