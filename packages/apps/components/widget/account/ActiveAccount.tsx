3f8bee7b777297382dca5c519dbcdd61b2d0cc18
sxlwar <sxlwar@163.com>
2022-08-17 17:54:18 +0800

    fix: memory lastest selected polkadot account (#172)

--- packages/apps/components/widget/account/ActiveAccount.tsx
+++ packages/apps/components/widget/account/ActiveAccount.tsx
@@ -1,16 +1,12 @@
 import { CloseCircleOutlined, DisconnectOutlined, LoadingOutlined, SettingFilled } from '@ant-design/icons';
 import { Badge, Button, message, Tooltip } from 'antd';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
 import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Icon } from 'shared/components/widget/Icon';
-import { isFormalChain, isTestChain } from 'shared/config/env';
-import { crabDVMConfig, darwiniaConfig, pangolinConfig } from 'shared/config/network';
-import { pangolinDVMConfig } from 'shared/config/network/pangolin-dvm';
-import { ChainConfig, ConnectionStatus, EthereumChainConfig, SupportedWallet } from 'shared/model';
+import { ConnectionStatus, EthereumChainConfig, SupportedWallet } from 'shared/model';
 import { switchEthereumChain } from 'shared/utils/connection';
-import { convertToSS58 } from 'shared/utils/helper';
+import { updateStorage } from 'shared/utils/helper';
 import { isPolkadotNetwork } from 'shared/utils/network';
 import { useAccount, useApi, useWallet } from '../../../providers';
 import { SelectAccountModal } from './SelectAccountModal';
@@ -20,15 +16,8 @@ const Identicon = dynamic(() => import('@polkadot/react-identicon'), {
   ssr: false,
 });
 
-export function ActiveAccount() {
-  const router = useRouter();
-  const isTransfer = router.pathname === '/';
-
-  return isTransfer ? <ActiveAccountStrict /> : <ActiveAccountNormal />;
-}
-
 // eslint-disable-next-line complexity
-function ActiveAccountStrict() {
+export function ActiveAccount() {
   const { departureConnection, departure, connectDepartureNetwork, isConnecting } = useApi();
   const { t } = useTranslation();
   const { account, setAccount } = useAccount();
@@ -131,6 +120,7 @@ function ActiveAccountStrict() {
         onCancel={() => setIsVisible(false)}
         onSelect={(acc) => {
           setAccount(acc);
+          updateStorage({ [departureConnection.type]: acc });
           setIsVisible(false);
         }}
         title={
@@ -153,125 +143,6 @@ function ActiveAccountStrict() {
           } else {
             message.error(t('The selected wallet does not match the token to be transferred'));
           }
-        }}
-        title={
-          <div className="inline-flex items-center space-x-1">
-            <span>{t('Switch wallet')}</span>
-          </div>
-        }
-        footer={null}
-      />
-    </>
-  );
-}
-
-// TODO: remove it; we do not need it anymore if the apps has no other page
-function ActiveAccountNormal() {
-  const { departureConnection, departure, connectDepartureNetwork, isConnecting } = useApi();
-  const { t } = useTranslation();
-  const { account: address, setAccount } = useAccount();
-  const [isVisible, setIsVisible] = useState(false);
-  const [isWalletVisible, setIsWalletVisible] = useState(false);
-
-  const disconnected = useMemo(() => {
-    const { type, status } = departureConnection;
-
-    return type !== 'unknown' && status !== ConnectionStatus.success;
-  }, [departureConnection]);
-
-  const prefix = useMemo(() => (isFormalChain ? darwiniaConfig.ss58Prefix : pangolinConfig.ss58Prefix), []);
-  const account = useMemo(() => convertToSS58(address, prefix), [address, prefix]);
-
-  const accounts = useMemo(
-    () => departureConnection.accounts.map((item) => ({ ...item, address: convertToSS58(item.address, prefix) })),
-    [departureConnection.accounts, prefix]
-  );
-
-  return (
-    <>
-      {departureConnection.accounts.length >= 1 ? (
-        <>
-          <section className={`flex items-center relative`}>
-            {disconnected && (
-              <Badge
-                count={
-                  <Tooltip title={t('Wallet provider offline')}>
-                    <DisconnectOutlined className="text-red-500" />
-                  </Tooltip>
-                }
-                className="absolute -top-2 -right-2 z-20"
-              ></Badge>
-            )}
-
-            <Button
-              className={`flex items-center justify-around px-1 overflow-hidden`}
-              icon={<img src={`/image/${departureConnection.type}.svg`} width={18} height={18} />}
-              style={{ maxWidth: 200 }}
-              onClick={() => {
-                if (departureConnection.accounts.length > 1) {
-                  setIsVisible(true);
-                } else {
-                  setIsWalletVisible(true);
-                }
-              }}
-            >
-              <span className="truncate ml-1">{account}</span>
-
-              {departureConnection.accounts.length > 1 && <Icon name="down" className="w-8 h-8" />}
-            </Button>
-
-            <span className="lg:hidden flex">
-              <Identicon value={account} size={20} className="rounded-full border p-1" />
-            </span>
-          </section>
-
-          <SettingFilled
-            onClick={() => setIsVisible(true)}
-            className={`lg:hidden inline-flex items-center text-2xl h-8 text-${departure.name}-main`}
-          />
-        </>
-      ) : (
-        <Button
-          disabled={isConnecting}
-          icon={isConnecting && <LoadingOutlined />}
-          onClick={() => setIsWalletVisible(true)}
-        >
-          {t('Connect to Wallet')}
-        </Button>
-      )}
-
-      <SelectAccountModal
-        visible={isVisible}
-        defaultValue={account}
-        accounts={accounts}
-        onCancel={() => setIsVisible(false)}
-        onSelect={(acc) => {
-          setAccount(acc);
-          setIsVisible(false);
-        }}
-        title={
-          <div className="inline-flex items-center space-x-1">
-            <span>{t('Select active account')}</span>
-          </div>
-        }
-        footer={null}
-      />
-
-      <SelectWalletModal
-        visible={isWalletVisible}
-        defaultValue={departureConnection.type}
-        onCancel={() => setIsWalletVisible(false)}
-        onSelect={(wallet) => {
-          let config: ChainConfig;
-
-          if (wallet === 'metamask') {
-            config = isTestChain ? pangolinDVMConfig : crabDVMConfig;
-          } else {
-            config = isTestChain ? pangolinConfig : darwiniaConfig;
-          }
-
-          setIsWalletVisible(false);
-          connectDepartureNetwork(config);
         }}
         title={
           <div className="inline-flex items-center space-x-1">
