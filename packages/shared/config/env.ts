19402baa95f7bd6b1950d5264fbe49f22b1a9e11
sxlwar <sxlwar@163.com>
2022-08-29 12:07:46 +0800

    ci: add stg command

--- packages/shared/config/env.ts
+++ packages/shared/config/env.ts
@@ -1,11 +1,13 @@
 export const CHAIN_TYPE = process.env.CHAIN_TYPE || process.env.NEXT_PUBLIC_CHAIN_TYPE;
 export const ENV = process.env.NODE_ENV || process.env.REACT_APP_HOST_TYPE;
 export const ENDPOINT = (process.env.ENDPOINT as string) || (process.env.NEXT_PUBLIC_ENDPOINT as string);
+export const DEPLOYMENT_ENV = process.env.DEPLOYMENT_ENV || process.env.NEXT_PUBLIC_DEPLOYMENT_ENV;
 
 export const isFormalChain = CHAIN_TYPE === 'formal';
 export const isTestChain = CHAIN_TYPE === 'test';
 export const isDev = ENV === 'development';
 export const isProd = !isDev;
+export const isStg = DEPLOYMENT_ENV === 'stg';
 
 export const SUBSTRATE_DVM_WITHDRAW = (process.env.SUBSTRATE_DVM_WITHDRAW ||
   process.env.NEXT_PUBLIC_SUBSTRATE_DVM_WITHDRAW) as string;
