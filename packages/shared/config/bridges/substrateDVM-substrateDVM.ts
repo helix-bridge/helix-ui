446e9a26c6f32b987dbd75f9e1186a340947c63c
sxlwar <sxlwar@163.com>
2022-09-02 11:39:57 +0800

    chore: disable

--- packages/shared/config/bridges/substrateDVM-substrateDVM.ts
+++ packages/shared/config/bridges/substrateDVM-substrateDVM.ts
@@ -23,6 +23,8 @@ export const darwiniaDVMDarwiniaDVMConfig: SubstrateDVMSubstrateDVMBridgeConfig 
 export const darwiniaDVMDarwiniaDVM = new Bridge(darwiniaDVMConfig, darwiniaDVMConfig, darwiniaDVMDarwiniaDVMConfig, {
   name: 'substrateDVM-substrateDVM',
   category: 'helix',
+  disableIssue: true,
+  disableRedeem: true,
 });
 
 const pangoroDVMpangolinDVMConfig: SubstrateDVMSubstrateDVMBridgeConfig = {
