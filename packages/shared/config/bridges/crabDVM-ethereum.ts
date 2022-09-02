47bf8911a748a1e499ec92a7fb4714f01bea581d
xiaoch05 <xiaoch2010@gmail.com>
2022-09-02 17:23:30 +0800

    update cbridge OriginalTokenVault address (#226)

--- packages/shared/config/bridges/crabDVM-ethereum.ts
+++ packages/shared/config/bridges/crabDVM-ethereum.ts
@@ -7,7 +7,7 @@ const crabDVMEthereumConfig: CrabDVMEthereumBridgeConfig = {
     backing: '0x841ce48F9446C8E281D3F1444cB859b4A6D0738C',
     issuing: '0x5427FEFA711Eff984124bFBB1AB6fbf5E3DA1820',
     stablecoinBacking: '0x3b53D2C7B44d40BE05Fa5E2309FFeB6eB2492d88',
-    stablecoinIssuing: '0x7510792A3B1969F9307F3845CE88e39578f2bAE1',
+    stablecoinIssuing: '0xB37D31b2A74029B5951a2778F959282E2D518595',
   },
 };
 
--- packages/shared/config/bridges/ethereum-arbitrum.ts
+++ packages/shared/config/bridges/ethereum-arbitrum.ts
@@ -6,7 +6,7 @@ const ethereumArbitrumConfig: EthereumArbitrumBridgeConfig = {
   contracts: {
     backing: '0x5427FEFA711Eff984124bFBB1AB6fbf5E3DA1820',
     issuing: '0x1619DE6B6B20eD217a58d00f37B9d47C7663feca',
-    stablecoinBacking: '0x7510792A3B1969F9307F3845CE88e39578f2bAE1',
+    stablecoinBacking: '0xB37D31b2A74029B5951a2778F959282E2D518595',
   },
 };
 
