950847413491a027c5d4e3a6a95c22f3287d401f
sxlwar <sxlwar@163.com>
2022-09-15 17:18:08 +0800

    chore: network graph works

--- packages/shared/utils/network/network.ts
+++ packages/shared/utils/network/network.ts
@@ -1,4 +1,3 @@
-import upperFirst from 'lodash/upperFirst';
 import {
   knownDVMNetworks,
   knownEthereumNetworks,
@@ -22,19 +21,3 @@ export const isDVMNetwork = isSpecifyNetwork(knownDVMNetworks);
 export const isEthereumNetwork = isSpecifyNetwork(knownEthereumNetworks);
 
 export const isParachainNetwork = isSpecifyNetwork(knownParachainNetworks);
-
-export function getDisplayName(config: ChainConfig | null): string {
-  if (!config) {
-    return 'unknown';
-  }
-
-  if (isDVMNetwork(config.name)) {
-    return `${upperFirst(config.name.split('-')[0])} Smart Chain`;
-  }
-
-  if (config.name.includes('parachain')) {
-    return config.name.split('-').map(upperFirst).join(' ');
-  }
-
-  return config.fullName ?? `${upperFirst(config.name)} Chain`;
-}
