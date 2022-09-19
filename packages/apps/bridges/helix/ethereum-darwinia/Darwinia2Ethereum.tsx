f2fe659c3d463f9939bd540e7496641abe0588c9
sxlwar <sxlwar@163.com>
2022-09-19 14:55:11 +0800

    chore: restore e2d max ring

--- packages/apps/bridges/helix/ethereum-darwinia/Darwinia2Ethereum.tsx
+++ packages/apps/bridges/helix/ethereum-darwinia/Darwinia2Ethereum.tsx
@@ -71,13 +71,22 @@ export function Darwinia2Ethereum({
       } = data;
 
       const [ringBalance, ktonBalance] = balances ?? [];
+      let availableMaxRing = new BN(0);
+
+      if (ringBalance && fee) {
+        const max = ringBalance.sub(fee);
+
+        if (max.gt(new BN(0))) {
+          availableMaxRing = max;
+        }
+      }
 
       const validateObs = validate([fee, balances], {
         isRING: isRing(symbol),
         balance: isRing(symbol) ? ringBalance : ktonBalance,
         amount: new BN(toWei({ value: amount, decimals })),
         fee,
-        ringBalance,
+        ringBalance: availableMaxRing,
       });
 
       const beforeTransfer = validateObs.pipe(
