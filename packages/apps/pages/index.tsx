19109a0e0f1b1565854cbd771aa982babff2db43
sxlwar <sxlwar@163.com>
2022-09-23 15:04:51 +0800

    style: home page refactor (#269)

--- packages/apps/pages/index.tsx
+++ packages/apps/pages/index.tsx
@@ -1,98 +1,173 @@
-import { Spin } from 'antd';
-import { format, secondsToMilliseconds, subMilliseconds } from 'date-fns';
-import { useQuery } from 'graphql-hooks';
-import last from 'lodash/last';
-import orderBy from 'lodash/orderBy';
+import { GithubOutlined, GlobalOutlined, TwitterOutlined } from '@ant-design/icons';
 import { GetServerSidePropsContext } from 'next';
-import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import dynamic from 'next/dynamic';
-import { useMemo, useState } from 'react';
-import { DATE_FORMAT } from 'shared/config/constant';
-import { DailyStatistic, Network } from 'shared/model';
-import { prettyNumber } from 'shared/utils/helper/balance';
-import { gqlName } from 'shared/utils/helper/common';
-import { getBridge } from 'utils/bridge';
-import { getChainConfig } from 'utils/network';
-import { BarChart, Statistic } from '../components/dashboard/BarChart';
-import { Statistics } from '../components/dashboard/Statistics';
-import { STATISTICS_QUERY, TIMEPAST } from '../config';
+import { CSSProperties } from 'react';
+import { Logo } from 'shared/components/widget/Logo';
+import { BridgeCategory, ChainConfig, Social } from 'shared/model';
+import { bridgeCategoryDisplay } from '../utils/bridge/bridge';
+import { chainConfigs } from '../utils/network';
+
+interface Bridge {
+  category: BridgeCategory;
+  social: Social;
+  supportChains: ChainConfig[];
+  description: {
+    security: string;
+    tech: string;
+    developer: string;
+  };
+}
 
 const NetworkGraph = dynamic(() => import('../components/NetworkG6'), { ssr: false });
 
-function Page() {
-  const { t } = useTranslation('common');
-  const [loading] = useState(false);
+const h3Style: CSSProperties = {
+  fontFamily: 'IBM Plex Sans',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  fontSize: 50,
+  textAlign: 'right',
+  textTransform: 'capitalize',
+  margin: 0,
+  lineHeight: 1.3,
+};
 
-  const { data: response } = useQuery(STATISTICS_QUERY, {
-    variables: { timepast: TIMEPAST },
-  });
+const pStyle: CSSProperties = {
+  fontFamily: 'IBM Plex Sans',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: 24,
+  textAlign: 'right',
+  opacity: 0.7,
+  margin: 0,
+  lineHeight: 1.3,
+};
 
-  const dailyStatistics = useMemo(
-    () => (response ? (response[gqlName(STATISTICS_QUERY)] as DailyStatistic[]) : []),
-    [response]
+const getSupportChains = (category: BridgeCategory) =>
+  chainConfigs.filter((config) =>
+    config.tokens
+      .map((token) => token.cross)
+      .flat()
+      .find((item) => item.category === category)
   );
+const bridges: Bridge[] = [
+  {
+    category: 'helix',
+    social: {
+      portal: 'https://darwinia.network/',
+      github: 'https://github.com/helix-bridge',
+      twitter: 'https://twitter.com/DarwiniaNetwork',
+    },
+    supportChains: getSupportChains('helix'),
+    description: {
+      security: 'Trust-less',
+      tech: 'Light Client & Relays Bridge',
+      developer: 'Helix',
+    },
+  },
+  {
+    category: 'cBridge',
+    social: {
+      portal: 'https://cbridge.celer.network/',
+      github: 'https://github.com/celer-network',
+      twitter: 'https://twitter.com/CelerNetwork',
+    },
+    supportChains: getSupportChains('cBridge'),
+    description: {
+      security: 'Trust-less',
+      tech: 'Liquidity Networks',
+      developer: 'Celer',
+    },
+  },
+  {
+    category: 'XCM',
+    social: {
+      portal: 'https://polkadot.js.org/',
+      github: 'https://github.com/polkadot-js',
+      twitter: 'https://twitter.com/polkadot',
+    },
+    supportChains: getSupportChains('XCM'),
+    description: {
+      security: 'Trust-less',
+      tech: 'Light Client & Relays Bridge',
+      developer: 'Polkadot',
+    },
+  },
+];
 
-  const { transactions, transactionsTotal } = useMemo(() => {
-    if (!dailyStatistics) {
-      return { volume: [], transactions: [], volumeTotal: 0, transactionsTotal: 0 };
-    }
-
-    return {
-      transactions: dailyStatistics
-        .map(({ timestamp, dailyCount }) => [secondsToMilliseconds(+timestamp), +dailyCount])
-        .reverse() as Statistic[],
-      transactionsTotal: prettyNumber(
-        dailyStatistics.reduce((acc, cur) => acc + Number(cur.dailyCount), 0),
-        { decimal: 0 }
-      ),
-    };
-  }, [dailyStatistics]);
-
-  const startTime = useMemo(() => {
-    const date = !dailyStatistics?.length
-      ? subMilliseconds(Date.now(), secondsToMilliseconds(TIMEPAST)).getTime()
-      : secondsToMilliseconds(+last(dailyStatistics)!.timestamp);
-
-    return format(date, DATE_FORMAT) + ' (+UTC)';
-  }, [dailyStatistics]);
-
-  const transactionsRank = useMemo(() => {
-    const data = Object.entries(
-      dailyStatistics?.reduce((acc, cur) => {
-        const bridge = getBridge([cur.fromChain as Network, cur.toChain as Network]);
-        const key = bridge.issue.join('_');
-
-        acc[key] = (acc[key] ?? 0) + Number(cur.dailyCount);
-
-        return acc;
-      }, {} as { [key: string]: number })
-    ).map((item) => {
-      const [fromChain, toChain] = item[0].split('_') as [Network, Network];
-      const total = item[1];
-
-      return { fromChain: getChainConfig(fromChain), toChain: getChainConfig(toChain), total };
-    });
-
-    return orderBy(data, ['total'], ['desc']);
-  }, [dailyStatistics]);
-
+function Page() {
   return (
     <div>
-      <Statistics title={t('transactions')} startTime={startTime} total={transactionsTotal} rank={transactionsRank}>
-        {loading ? (
-          <div className="block relative top-1/3 text-center">
-            <Spin />
-          </div>
-        ) : (
-          <BarChart data={transactions} name="transactions" />
-        )}
-      </Statistics>
+      <div className="2xl:mb-24 lg:mb-8">
+        <h3 style={h3Style}>Helix</h3>
+        <h3 style={h3Style}>Aggregators of</h3>
+        <h3 style={h3Style}>strictly selected bridges</h3>
+
+        <p style={pStyle}>Providing a secure and convenient service for asset cross-chain users</p>
+      </div>
+
+      <h2 className="text-4xl font-bold">Supported Chains</h2>
 
       <div className="gap-4 lg:gap-6">
-        <h2 className="uppercase text-xl font-normal my-6">{t<string>('Bridges')}</h2>
-
         <NetworkGraph />
+      </div>
+
+      <h2 className="text-4xl font-bold mt-16 mb-8">Aggregated Bridges</h2>
+
+      <div className={`grid grid-cols-3 gap-8`}>
+        {bridges.map((bridge) => (
+          <div key={bridge.category} className="bg-antDark p-8">
+            <div className="flex items-center gap-2">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={`/image/bridges/${bridge.category}-bridge.png`} className={`w-8 md:w-12`} />
+              <div>
+                <h4 className="text-xl">{bridgeCategoryDisplay(bridge.category)}</h4>
+                <div className="flex items-center gap-2">
+                  <a
+                    href={bridge.social.portal}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="flex text-gray-400 hover:text-gray-200"
+                  >
+                    <GlobalOutlined style={{ fontSize: '1.25rem' }} />
+                  </a>
+
+                  <a
+                    href={bridge.social.github}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="flex text-gray-400 hover:text-gray-200"
+                  >
+                    <GithubOutlined style={{ fontSize: '1.25rem' }} />
+                  </a>
+
+                  <a
+                    href={bridge.social.twitter}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="flex text-gray-400 hover:text-gray-200"
+                  >
+                    <TwitterOutlined style={{ fontSize: '1.25rem' }} />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <hr className="opacity-20 my-4" />
+
+            <div className="flex flex-nowrap items-center gap-2 w-full overflow-x-scroll mb-4">
+              {bridge.supportChains.map((chain) => (
+                <Logo name={chain.logos[0].name} width={24} height={24} key={chain.name} />
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-px text-gray-400">
+              <span>{`Security: ${bridge.description.security}`}</span>
+              <span>{`Technology: ${bridge.description.tech}`}</span>
+              <span>{`Bridge Developer: ${bridge.description.developer}`}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
