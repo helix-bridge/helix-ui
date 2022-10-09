62ec0f557789e2950493d6c93a3cd4b5e344314c
sxlwar <sxlwar@163.com>
2022-10-09 14:54:56 +0800

    chore: unknown-unavailable

---
+++ packages/apps/bridges/unknown-unavailable/utils/bridge.ts
@@ -0,0 +1,30 @@
+import { EMPTY, Observable } from 'rxjs';
+import { crabConfig, pangoroConfig } from 'shared/config/network';
+import { BridgeBase, BridgeConfig, ChainConfig, Tx } from 'shared/model';
+import { Bridge } from '../../../model/bridge';
+
+export class UnknownUnavailableBridge extends Bridge<BridgeConfig, ChainConfig, ChainConfig> {
+  back(): Observable<Tx> {
+    return EMPTY;
+  }
+
+  burn(): Observable<Tx> {
+    return EMPTY;
+  }
+
+  genTxParamsValidations(): [boolean, string][] {
+    return [];
+  }
+}
+
+export const unknownUnavailable = new BridgeBase(
+  pangoroConfig,
+  crabConfig,
+  {},
+  {
+    category: 'helix',
+    name: 'substrate-DVM',
+    issueCompName: 'Unknown2Unavailable',
+    redeemCompName: 'Unavailable2Unknown',
+  }
+);
