57b6e1c0ab58c74073ff154547bea05fc0027a75
sxlwar <sxlwar@163.com>
2022-10-10 14:33:10 +0800

    chore: bump panoglin pangoro spec version 210000

--- packages/shared/config/network/pangoro.ts
+++ packages/shared/config/network/pangoro.ts
@@ -14,7 +14,7 @@ export const pangoroConfig: PolkadotChainConfig = {
     twitter: 'https://twitter.com/DarwiniaNetwork',
   },
   ss58Prefix: 18,
-  specVersion: 29050,
+  specVersion: 210000,
   tokens: [
     {
       name: 'ORING',
