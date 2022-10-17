4810e7a218b4e1ff3809d5d7ed8f48191efd1788
sxlwar <sxlwar@163.com>
2022-10-17 10:02:48 +0800

    chore: unit test

--- packages/apps/__tests__/components/CrossChainInfo.spec.tsx
+++ packages/apps/__tests__/components/CrossChainInfo.spec.tsx
@@ -2,14 +2,24 @@
 
 import { Typography } from 'antd';
 import { create } from 'react-test-renderer';
+import { darwiniaConfig, darwiniaDVMConfig } from 'shared/config/network';
+import { BridgeConfig, ContractConfig, ChainConfig, CrossChainDirection } from 'shared/model';
+import { isRing } from 'shared/utils/helper/validator';
 import { darwiniaDarwiniaDVM } from '../../bridges/helix/substrate-dvm/config';
 import { CrossChainInfo } from '../../components/widget/CrossChainInfo';
+import { Bridge } from '../../core/bridge';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key: string) => key }),
 }));
 
 describe('<CrossChainInfo />', () => {
+  const fromToken = darwiniaConfig.tokens.find((item) => isRing(item.symbol))!;
+  const toToken = darwiniaDVMConfig.tokens.find((item) => isRing(item.symbol))!;
+  const direction: CrossChainDirection = {
+    from: { ...fromToken, meta: darwiniaConfig, amount: '8' },
+    to: { ...toToken, meta: darwiniaDVMConfig, amount: '8' },
+  };
   beforeAll(() => {
     Object.defineProperty(window, 'matchMedia', {
       writable: true,
@@ -27,7 +37,13 @@ describe('<CrossChainInfo />', () => {
   });
 
   it('render cross-chain item', () => {
-    const component = create(<CrossChainInfo bridge={darwiniaDarwiniaDVM} fee={{ amount: '8', symbol: 'RING' }} />);
+    const component = create(
+      <CrossChainInfo
+        bridge={darwiniaDarwiniaDVM as Bridge<BridgeConfig<ContractConfig>, ChainConfig, ChainConfig>}
+        direction={direction}
+        fee={{ amount: 8, symbol: 'RING' }}
+      />
+    );
 
     let tree = component.toJSON();
 
@@ -36,7 +52,11 @@ describe('<CrossChainInfo />', () => {
 
   it('render cross-chain with children', () => {
     const component = create(
-      <CrossChainInfo bridge={darwiniaDarwiniaDVM} fee={{ amount: '8', symbol: 'RING' }}>
+      <CrossChainInfo
+        bridge={darwiniaDarwiniaDVM as Bridge<BridgeConfig<ContractConfig>, ChainConfig, ChainConfig>}
+        direction={direction}
+        fee={{ amount: 8, symbol: 'RING' }}
+      >
         <span>can receive children node</span>
       </CrossChainInfo>
     );
@@ -49,8 +69,9 @@ describe('<CrossChainInfo />', () => {
   it('render cross-chain and extra nodes', () => {
     const component = create(
       <CrossChainInfo
-        bridge={darwiniaDarwiniaDVM}
-        fee={{ amount: '8', symbol: 'RING' }}
+        bridge={darwiniaDarwiniaDVM as Bridge<BridgeConfig<ContractConfig>, ChainConfig, ChainConfig>}
+        direction={direction}
+        fee={{ amount: 8, symbol: 'RING' }}
         extra={[
           {
             name: 'Allowance',
@@ -80,7 +101,13 @@ describe('<CrossChainInfo />', () => {
   });
 
   it('render cross-chain with dynamic fee', () => {
-    const component = create(<CrossChainInfo bridge={darwiniaDarwiniaDVM} isDynamicFee />);
+    const component = create(
+      <CrossChainInfo
+        bridge={darwiniaDarwiniaDVM as Bridge<BridgeConfig<ContractConfig>, ChainConfig, ChainConfig>}
+        direction={direction}
+        isDynamicFee
+      />
+    );
 
     let tree = component.toJSON();
 
