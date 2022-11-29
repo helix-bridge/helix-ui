d99b4057ad46f531e60572f66dbc7efe1090bcc1
sxlwar <sxlwar@163.com>
2022-11-29 12:21:27 +0800

    chore: check shiden khala burn tx

--- packages/apps/bridges/xcm/shiden-khala/utils/bridge.ts
+++ packages/apps/bridges/xcm/shiden-khala/utils/bridge.ts
@@ -3,6 +3,8 @@ import omit from 'lodash/omit';
 import type { Observable } from 'rxjs';
 import { CrossChainDirection, CrossToken, ParachainChainConfig, Tx } from 'shared/model';
 import { entrance, waitUntilConnected } from 'shared/utils/connection';
+import { convertToDvm } from 'shared/utils/helper/address';
+import { signAndSendExtrinsic } from 'shared/utils/tx/polkadot';
 import { Bridge, TokenWithAmount } from '../../../../core/bridge';
 import { IssuingPayload, RedeemPayload, ShidenKhalaBridgeConfig } from '../model';
 
@@ -14,7 +16,48 @@ export class ShidenKhalaBridge extends Bridge<ShidenKhalaBridgeConfig, Parachain
   }
 
   burn(payload: RedeemPayload): Observable<Tx> {
-    return this.xcmReserveTransferAssets(payload);
+    const {
+      direction: { from: departure, to: arrival },
+      sender,
+      recipient,
+      wallet,
+    } = payload;
+    const amount = this.wrapXCMAmount(departure);
+    const api = entrance.polkadot.getInstance(departure.meta.provider.wss);
+
+    const asset = api.createType('XcmV1MultiAsset', {
+      id: api.createType('XcmV1MultiassetAssetId', {
+        Concrete: api.createType('XcmV1MultiLocation', {
+          parents: 0,
+          interior: api.createType('XcmV1MultilocationJunctions', 'Here'),
+        }),
+      }),
+      fun: api.createType('XcmV1MultiassetFungibility', {
+        Fungible: api.createType('Compact<u128>', amount),
+      }),
+    });
+
+    const dest = api.createType('XcmV1MultiLocation', {
+      parents: 1,
+      interior: api.createType('XcmV1MultilocationJunctions', {
+        X2: [
+          api.createType('XcmV1Junction', {
+            Parachain: api.createType('Compact<u32>', arrival.meta.paraId),
+          }),
+          api.createType('XcmV1Junction', {
+            AccountId32: {
+              network: api.createType('XcmV0JunctionNetworkId', 'Any'),
+              id: convertToDvm(recipient),
+            },
+          }),
+        ],
+      }),
+    });
+
+    const destWeight = 6e9;
+    const extrinsic = api.tx.xTransfer.transfer(asset, dest, destWeight);
+
+    return signAndSendExtrinsic(api, sender, extrinsic, wallet);
   }
 
   async getFee(
--- packages/apps/core/bridge.ts
+++ packages/apps/core/bridge.ts
@@ -255,14 +255,22 @@ export abstract class Bridge<
     const api = entrance.polkadot.getInstance(departure.meta.provider.wss);
 
     const dest = api.createType('XcmVersionedMultiLocation', {
-      V1: api.createType('XcmV1MultiLocation', {
-        parents: 1,
-        interior: api.createType('XcmV1MultilocationJunctions', {
-          X1: api.createType('XcmV1Junction', {
-            Parachain: api.createType('Compact<u32>', arrival.meta.paraId),
-          }),
-        }),
-      }),
+      V1: api.createType(
+        'XcmV1MultiLocation',
+        this.isIssue(departure.host, arrival.host)
+          ? {
+              parents: 1,
+              interior: api.createType('XcmV1MultilocationJunctions', {
+                X1: api.createType('XcmV1Junction', {
+                  Parachain: api.createType('Compact<u32>', arrival.meta.paraId),
+                }),
+              }),
+            }
+          : {
+              parents: 0,
+              interior: api.createType('XcmV1MultilocationJunctions', 'Here'),
+            }
+      ),
     });
 
     const beneficiary = api.createType('XcmVersionedMultiLocation', {
