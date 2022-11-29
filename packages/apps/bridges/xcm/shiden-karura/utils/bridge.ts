a3e544b2dc1492ae0af6a3ff56eff31fd34fdc6b
sxlwar <sxlwar@163.com>
2022-11-29 13:55:32 +0800

    chore: shiden<>karura burn

--- packages/apps/bridges/xcm/shiden-karura/utils/bridge.ts
+++ packages/apps/bridges/xcm/shiden-karura/utils/bridge.ts
@@ -2,6 +2,9 @@ import { BN } from '@polkadot/util';
 import omit from 'lodash/omit';
 import type { Observable } from 'rxjs';
 import { ChainConfig, CrossChainDirection, CrossToken, Tx } from 'shared/model';
+import { entrance } from 'shared/utils/connection';
+import { convertToDvm } from 'shared/utils/helper/address';
+import { signAndSendExtrinsic } from 'shared/utils/tx';
 import { Bridge, TokenWithAmount } from '../../../../core/bridge';
 import { IssuingPayload, RedeemPayload, ShidenKaruraBridgeConfig } from '../model';
 
@@ -13,7 +16,42 @@ export class ShidenKaruraBridge extends Bridge<ShidenKaruraBridgeConfig, ChainCo
   }
 
   burn(payload: RedeemPayload): Observable<Tx> {
-    return this.xcmReserveTransferAssets(payload);
+    const {
+      direction: { from: departure, to: arrival },
+      sender,
+      recipient,
+      wallet,
+    } = payload;
+    const amount = this.wrapXCMAmount(departure);
+    const api = entrance.polkadot.getInstance(departure.meta.provider.wss);
+
+    const currencyId = api.createType('AcalaPrimitivesCurrencyCurrencyId', {
+      ForeignAsset: departure.address,
+    });
+
+    const dest = api.createType('XcmVersionedMultiLocation', {
+      V1: api.createType('XcmV1MultiLocation', {
+        parents: 1,
+        interior: api.createType('XcmV1MultilocationJunctions', {
+          X2: [
+            api.createType('XcmV1Junction', {
+              Parachain: api.createType('Compact<u32>', arrival.meta.paraId),
+            }),
+            api.createType('XcmV1Junction', {
+              AccountId32: {
+                network: api.createType('XcmV0JunctionNetworkId', 'Any'),
+                id: convertToDvm(recipient),
+              },
+            }),
+          ],
+        }),
+      }),
+    });
+
+    const destWeight = 5_000_000_000;
+    const extrinsic = api.tx.xTokens.transfer(currencyId, amount, dest, destWeight);
+
+    return signAndSendExtrinsic(api, sender, extrinsic, wallet);
   }
 
   async getFee(
@@ -21,14 +59,8 @@ export class ShidenKaruraBridge extends Bridge<ShidenKaruraBridgeConfig, ChainCo
   ): Promise<TokenWithAmount | null> {
     const { from, to } = direction;
     const token = omit(direction.from, ['amount', 'meta']);
+    const amount = this.isIssue(from.host, to.host) ? new BN('926960000000000') : new BN('4635101625000000');
 
-    if (this.isIssue(from.host, to.host)) {
-      return {
-        ...token,
-        amount: new BN('926960000000000'),
-      } as TokenWithAmount;
-    } else {
-      return { ...token, amount: new BN('4635101625000000') } as TokenWithAmount;
-    }
+    return { ...token, amount } as TokenWithAmount;
   }
 }
