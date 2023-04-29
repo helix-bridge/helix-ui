00d591cfa37f61666db27b0a52395ad7fd8a5a23
xiaoch05 <xiaoch2010@gmail.com>
2023-04-29 12:06:35 +0800

    repair unit test

--- packages/shared/__tests__/helper/url.spec.ts
+++ packages/shared/__tests__/helper/url.spec.ts
@@ -6,25 +6,17 @@ import { validateDirection } from '../../utils/helper/url';
 
 describe('url utils', () => {
   it('validate direction', () => {
-    const ropstenRing = { ...ropstenConfig.tokens[0], amount: '', meta: ropstenConfig  }
-    const ropstenKton = { ...ropstenConfig.tokens[1], amount: '', meta: ropstenConfig  }
-    const pangolinRing = { ...pangolinConfig.tokens[0], amount: '', meta: pangoroConfig };
-    const pangolinKton = { ...pangolinConfig.tokens[1], amount: '', meta: pangolinConfig };
+    const ropstenRing = { ...ropstenConfig.tokens[0], amount: '', meta: ropstenConfig };
+    const ropstenKton = { ...ropstenConfig.tokens[1], amount: '', meta: ropstenConfig };
 
     const res1 = validateDirection({ from: null, to: null });
     const res2 = validateDirection({ from: ropstenRing, to: null });
     const res3 = validateDirection({ from: ropstenRing, to: ropstenRing });
-    const res4 = validateDirection({ from: ropstenKton, to: ropstenKton })
-    const res5 = validateDirection({ from: ropstenRing, to: pangolinRing });
-    const res6 = validateDirection({ from: ropstenKton, to: pangolinRing });
-    const res7 = validateDirection({ from: ropstenKton, to: pangolinKton });
+    const res4 = validateDirection({ from: ropstenKton, to: ropstenKton });
 
     expect(res1).toEqual(DEFAULT_DIRECTION);
     expect(res2).toEqual({ from: ropstenRing, to: DEFAULT_DIRECTION.to });
     expect(res3).toEqual(DEFAULT_DIRECTION);
     expect(res4).toEqual(DEFAULT_DIRECTION);
-    expect(res5).toEqual({ from: ropstenRing, to: pangolinRing });
-    expect(res6).toEqual(DEFAULT_DIRECTION);
-    expect(res7).toEqual({ from: ropstenKton, to: pangolinKton });
   });
 });
