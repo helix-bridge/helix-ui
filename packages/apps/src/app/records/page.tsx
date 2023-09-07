779aafd14ddcdae64e1cabd0ac50b16e54ad6fa9
JayJay1024 <lygm1062@gmail.com>
2023-09-07 16:38:13 +0800

    tabs ui component

--- packages/apps/src/app/records/page.tsx
+++ packages/apps/src/app/records/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import Tabs, { TabsProps } from "@/ui/tabs";
+import { useState } from "react";
+
+type TabKey = "all" | "pending" | "success" | "refunded";
+
 export default function Records() {
+  const [activeKey, setActiveKey] = useState<TabsProps<TabKey>["activeKey"]>("all");
+
   return (
     <main className="app-main">
-      <p>Records Page</p>
+      <div className="px-middle container mx-auto">
+        <Tabs
+          activeKey={activeKey}
+          onChange={setActiveKey}
+          items={[
+            {
+              key: "all",
+              label: <span>All</span>,
+              children: <div>All</div>,
+            },
+            {
+              key: "pending",
+              label: <span>Pending</span>,
+              children: <div>Pending</div>,
+            },
+            {
+              key: "success",
+              label: <span>Success</span>,
+              children: <div>Success</div>,
+            },
+            {
+              key: "refunded",
+              label: <span>Refunded</span>,
+              children: <div>Refunded</div>,
+            },
+          ]}
+        />
+      </div>
     </main>
   );
 }
---
+++ packages/apps/src/ui/tabs.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import { Key, ReactElement, useEffect, useRef, useState } from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+
+export interface TabsProps<K> {
+  activeKey: K;
+  items: {
+    key: K;
+    label: ReactElement;
+    children: ReactElement;
+  }[];
+  onChange?: (key: K) => void;
+}
+
+export default function Tabs<K extends Key = string>({ items, activeKey, onChange = () => undefined }: TabsProps<K>) {
+  const activeItem = items.find(({ key }) => key === activeKey) || items.at(0);
+
+  const [dividerWidth, setDividerWidth] = useState<number | undefined>();
+  const labelRefs = useRef<(HTMLButtonElement | null)[]>([]);
+  const railRef = useRef<HTMLDivElement | null>(null);
+
+  const previousRef = useRef<HTMLDivElement | null>(null);
+  const currentRef = useRef<HTMLDivElement | null>(null);
+  const stateRef = useRef(activeKey);
+
+  const nodeRef = stateRef.current === activeKey ? currentRef : previousRef;
+  stateRef.current = activeKey;
+
+  useEffect(() => {
+    const labelElement = labelRefs.current[items.findIndex(({ key }) => key === activeKey)];
+    const railElement = railRef.current;
+
+    if (labelElement && railElement) {
+      railElement.style.transform = `translateX(${labelElement.offsetLeft}px)`;
+      railElement.style.width = `${labelElement.clientWidth}px`;
+    }
+  }, [items, activeKey]);
+
+  return (
+    <div>
+      {/* labels */}
+      <div className="overflow-x-auto">
+        <div className="relative flex items-center gap-10" ref={(node) => setDividerWidth(node?.scrollWidth)}>
+          {items.map(({ key, label }, index) => (
+            <button
+              key={key}
+              type="button"
+              ref={(node) => (labelRefs.current[index] = node)}
+              onClick={(e) => {
+                e.stopPropagation();
+                onChange(key);
+              }}
+              className={`hover:text-primary/80 text-sm transition-all duration-200 active:opacity-60 ${
+                key === activeKey ? "text-primary font-bold" : "font-light text-white"
+              }`}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+
+        {/* divider & rail */}
+        <div className="mt-middle relative" style={{ width: dividerWidth }}>
+          <div className="bg-primary absolute h-[2px] w-6 transition-transform duration-200" ref={railRef} />
+          <div className="h-[1px] bg-transparent" />
+          <div className="h-[1px] bg-white/20" />
+        </div>
+      </div>
+
+      {/* content */}
+      <SwitchTransition>
+        <CSSTransition timeout={200} key={activeKey} nodeRef={nodeRef} classNames="tabs-fade" unmountOnExit>
+          <div ref={nodeRef} className="mt-middle overflow-x-auto">
+            {activeItem ? (
+              activeItem.children
+            ) : (
+              <div className="gap-large flex flex-col items-center pt-10">
+                <Image width={50} height={63} alt="No data" src="/images/no-data.svg" />
+                <span className="text-sm font-light text-white/50">No data</span>
+              </div>
+            )}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </div>
+  );
+}
