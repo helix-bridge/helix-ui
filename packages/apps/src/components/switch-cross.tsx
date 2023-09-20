07665a922be008b1fac2e5399ebacb1e0f5bea25
JayJay1024 <lygm1062@gmail.com>
2023-09-20 19:53:58 +0800

    update tips for unavailable cross-chain

--- packages/apps/src/components/switch-cross.tsx
+++ packages/apps/src/components/switch-cross.tsx
@@ -4,7 +4,10 @@ import { ButtonHTMLAttributes } from "react";
 
 export default function SwitchCross({ disabled, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <Tooltip content={<span className="text-xs font-normal text-white">Currently unavailable</span>} enabled={disabled}>
+    <Tooltip
+      content={<span className="text-xs font-normal text-white">This cross-chain is currently unavailable</span>}
+      enabled={disabled}
+    >
       <button
         className="transition hover:scale-105 hover:opacity-80 active:scale-95 disabled:scale-100 disabled:cursor-not-allowed disabled:opacity-100"
         disabled={disabled}
