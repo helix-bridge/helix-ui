c73774dad9cb52e5c571427b6c9ee6a328701930
JayJay1024 <lygm1062@gmail.com>
2023-10-03 18:17:36 +0800

    improve token-select logic

--- packages/apps/src/components/token-select.tsx
+++ packages/apps/src/components/token-select.tsx
@@ -16,12 +16,21 @@ import { useState } from "react";
 interface Props {
   options: TokenSymbol[];
   // network?: Network;
+  disabled?: boolean;
   value?: TokenSymbol;
   placeholder?: string;
-  onChange?: (value: TokenSymbol) => void;
+  className?: string;
+  onChange?: (value: TokenSymbol | undefined) => void;
 }
 
-export default function TokenSelect({ options, value, placeholder, onChange }: Props) {
+export default function TokenSelect({
+  options,
+  disabled,
+  value,
+  placeholder,
+  className,
+  onChange = () => undefined,
+}: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
   const { refs, context, floatingStyles } = useFloating({
@@ -49,27 +58,52 @@ export default function TokenSelect({ options, value, placeholder, onChange }: P
 
   return (
     <>
-      <div
-        className="gap-small border-line px-middle py-small flex items-center justify-between rounded border"
+      <button
+        className={`disabled:border-line gap-small border-line hover:border-primary flex items-center justify-between rounded border transition-colors duration-300 disabled:cursor-not-allowed ${className}`}
         ref={refs.setReference}
         {...getReferenceProps()}
+        disabled={disabled}
       >
         <span className={`text-sm font-normal ${value ? "text-white" : "text-white/50"}`}>{value || placeholder}</span>
-        <Image
-          src="/images/caret-down.svg"
-          alt="Caret down"
-          width={16}
-          height={16}
-          style={{ transform: isOpen ? "rotateX(180deg)" : "rotateX(0)" }}
-        />
-      </div>
+
+        <div className="gap-small flex shrink-0 items-center">
+          {value ? (
+            <button
+              className="relative h-[16px] w-[16px] shrink-0 rounded-full bg-transparent p-[2px] transition hover:scale-105 hover:bg-white/10 active:scale-95"
+              onClick={(e) => {
+                e.stopPropagation();
+                onChange(undefined);
+              }}
+            >
+              <Image alt="Close" fill src="/images/close.svg" />
+            </button>
+          ) : null}
+          <Image
+            src="/images/caret-down.svg"
+            alt="Caret down"
+            width={16}
+            height={16}
+            className="shrink-0"
+            style={{ transform: isOpen ? "rotateX(180deg)" : "rotateX(0)" }}
+          />
+        </div>
+      </button>
 
       {isMounted && (
         <FloatingPortal>
           <div style={floatingStyles} ref={refs.setFloating} {...getFloatingProps()} className="z-20">
             <div style={styles} className="bg-component border-line py-small flex flex-col rounded border">
               {options.map((option) => (
-                <button key={option}>{option}</button>
+                <button
+                  key={option}
+                  onClick={() => {
+                    onChange(option);
+                    setIsOpen(false);
+                  }}
+                  className="py-small px-large hover:text-primary text-start text-sm transition-colors"
+                >
+                  {option}
+                </button>
               ))}
             </div>
           </div>
