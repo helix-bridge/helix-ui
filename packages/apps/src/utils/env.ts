b63ce7b1d02ece4c0622723905bea6c4cb455d8d
JayJay1024 <lygm1062@gmail.com>
2023-10-18 10:01:41 +0800

    rename NEXT_PUBLIC_APP_ENV

--- packages/apps/src/utils/env.ts
+++ packages/apps/src/utils/env.ts
@@ -1,11 +1,11 @@
 export function isProduction() {
-  return process.env.NEXT_PUBLIC_NODE_ENV === "production";
+  return process.env.NEXT_PUBLIC_APP_ENV === "production";
 }
 
 export function isDevelopment() {
-  return process.env.NEXT_PUBLIC_NODE_ENV === "development";
+  return process.env.NEXT_PUBLIC_APP_ENV === "development";
 }
 
 export function isTest() {
-  return process.env.NEXT_PUBLIC_NODE_ENV === "test";
+  return process.env.NEXT_PUBLIC_APP_ENV === "test";
 }
