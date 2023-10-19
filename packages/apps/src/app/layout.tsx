f2e15fd43a06ca3ba9ef3eca3de8ab5999badb65
JayJay1024 <lygm1062@gmail.com>
2023-10-19 12:04:29 +0800

    improve scroll background color

--- packages/apps/src/app/layout.tsx
+++ packages/apps/src/app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="page-bg text-white">
-      <body className={ibm_plex_sans.className}>
+    <html lang="en" style={{ backgroundColor: "#000421" }}>
+      <body className={`${ibm_plex_sans.className} page-bg text-white`}>
         <GraphqlProvider>
           <RainbowProvider>
             <AppProvider>
