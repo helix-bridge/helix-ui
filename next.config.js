648b7ff2637940713229af4b325852527f3165ae
JayJay1024 <lygm1062@gmail.com>
2023-12-12 19:10:19 +0800

    Update distDir

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "/packages/apps/.next",
   webpack: (config) => {
     config.externals.push("lokijs", "encoding", "pino-pretty");
     return config;
