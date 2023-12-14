677e16f411abced28b002f6458c4ce962819a9c9
JayJay1024 <lygm1062@gmail.com>
2023-12-14 18:12:54 +0800

    Configure contracts of XTokenV3Bridge

--- src/bridges/xtoken-v3.ts
+++ src/bridges/xtoken-v3.ts
@@ -11,6 +11,14 @@ export class XTokenV3Bridge extends BaseBridge {
       symbol: "helix-symbol.svg",
     };
     this.name = "XTokenV3";
+
+    this.initContract();
+  }
+
+  private initContract() {
+    const backing = "0xb137BDf1Ad5392027832f54a4409685Ef52Aa9dA";
+    const issuing = "0x44A001aF6AcD2d5f5cB82FCB14Af3d497D56faB4";
+    this.initContractByBackingIssuing(backing, issuing);
   }
 
   protected async _transfer(
