407619dc0c0b9150cdda09b845430f408da6987d
JayJay1024 <lygm1062@gmail.com>
2024-02-08 16:09:36 +0800

    fix: lnv3 liquidity fee rate

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -5,3 +5,4 @@ export const FEE_RATE_BASE = 1000;
 
 export const FEE_RATE_MIN = 0;
 export const FEE_RATE_MAX = 0.25 * FEE_RATE_BASE; // 0.25%
+export const FEE_RATE_MAX_V3 = 100 * FEE_RATE_BASE; // 100%
