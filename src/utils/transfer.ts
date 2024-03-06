3d5d42160faedaffedf2d4790f0e469bcfe9fa2a
JayJay1024 <lygm1062@gmail.com>
2024-03-06 18:34:55 +0800

    feat: transfer utils

---
+++ src/utils/transfer.ts
@@ -0,0 +1,76 @@
+import { ChainConfig, Token, TokenCategory, TokenSymbol } from "@/types";
+import { getChainConfig, getChainConfigs } from ".";
+
+interface TokenOption {
+  image: string;
+  category: TokenCategory;
+  symbol: TokenSymbol;
+}
+const allTokenOptions: Record<Exclude<TokenCategory, "others">, TokenOption> = {
+  crab: { image: "crab.svg", category: "crab", symbol: "CRAB" },
+  eth: { image: "eth.svg", category: "eth", symbol: "ETH" },
+  ring: { image: "ring.svg", category: "ring", symbol: "RING" },
+  usdc: { image: "usdc.svg", category: "usdc", symbol: "USDC" },
+  usdt: { image: "usdt.svg", category: "usdt", symbol: "USDT" },
+};
+const sortedTokenCategories: Exclude<TokenCategory, "others">[] = ["usdt", "usdc", "eth", "ring", "crab"];
+const availableTokenCategories = new Set<TokenCategory>();
+const sourceChainOptions = new Map<TokenCategory, ChainConfig[]>();
+
+getChainConfigs()
+  .filter(({ hidden }) => !hidden)
+  .forEach((sourceChain) => {
+    sourceChain.tokens.forEach((sourceToken) => {
+      sourceToken.cross
+        .filter(({ hidden, bridge }) => !hidden && bridge.category === "lnbridge")
+        .forEach((cross) => {
+          const targetChain = getChainConfig(cross.target.network);
+          const targetToken = targetChain?.tokens.find(({ symbol }) => symbol === cross.target.symbol);
+
+          if (targetToken) {
+            availableTokenCategories.add(sourceToken.category);
+            sourceChainOptions.set(
+              sourceToken.category,
+              (sourceChainOptions.get(sourceToken.category) || [])
+                .filter(({ id }) => id !== sourceChain.id)
+                .concat(sourceChain),
+            );
+          }
+        });
+    });
+  });
+
+export function getTokenOptions() {
+  return sortedTokenCategories.filter((c) => availableTokenCategories.has(c)).map((c) => allTokenOptions[c]);
+}
+
+export function getSourceChainOptions(category: TokenCategory) {
+  return sourceChainOptions.get(category) || [];
+}
+
+export function getSourceTokenOptions(sourceChain: ChainConfig, tokenCategory: TokenCategory) {
+  return sourceChain.tokens.filter(
+    ({ category, cross }) =>
+      category === tokenCategory &&
+      cross.filter(({ hidden, bridge }) => !hidden && bridge.category === "lnbridge").length,
+  );
+}
+
+export function getTargetChainOptions(sourceToken: Token) {
+  return sourceToken.cross
+    .filter(({ hidden, bridge }) => !hidden && bridge.category === "lnbridge")
+    .map(({ target }) => getChainConfig(target.network))
+    .filter((c) => !c) as ChainConfig[];
+}
+
+export function getTargetTokenOptions(sourceToken: Token, targetChain: ChainConfig) {
+  return targetChain.tokens.filter(({ symbol }) =>
+    sourceToken.cross.some(
+      (c) =>
+        !c.hidden &&
+        c.target.symbol === symbol &&
+        c.target.network === targetChain.network &&
+        c.bridge.category === "lnbridge",
+    ),
+  );
+}
