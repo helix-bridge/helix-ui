4613284e79da8556be2bcab8c093247b5b3b121d
JayJay1024 <lygm1062@gmail.com>
2024-03-07 18:18:05 +0800

    feat: component TransferV2

---
+++ src/components/transfer-v2.tsx
@@ -0,0 +1,143 @@
+import { useCallback, useRef, useState } from "react";
+import TransferTokenSection from "./transfer-token-section";
+import {
+  getSourceChainOptions,
+  getSourceTokenOptions,
+  getTargetChainOptions,
+  getTargetTokenOptions,
+  getTokenOptions,
+} from "@/utils";
+import TransferChainSection from "./transfer-chain-section";
+import TransferAmountSection from "./transfer-amount-section";
+import TransferInformationSection from "./transfer-information-section";
+
+const tokenOptions = getTokenOptions();
+
+export default function TransferV2() {
+  const [token, setToken] = useState(tokenOptions[0]);
+  const [amount, setAmount] = useState({ input: "", value: 0n, valid: true });
+  const [sourceChain, setSourceChain] = useState(getSourceChainOptions(token.category)[0]);
+  const [sourceToken, setSourceToken] = useState(getSourceTokenOptions(sourceChain, token.category)[0]);
+  const [targetChain, setTargetChain] = useState(getTargetChainOptions(sourceToken)[0]);
+  const [targetToken, setTargetToken] = useState(getTargetTokenOptions(sourceToken, targetChain)[0]);
+
+  const tokenRef = useRef(token);
+  const sourceChainRef = useRef(sourceChain);
+  const sourceTokenRef = useRef(sourceToken);
+  const targetChainRef = useRef(targetChain);
+  const targetTokenRef = useRef(targetToken);
+
+  const handleTokenChange = useCallback((_token: typeof token) => {
+    setToken(_token);
+    tokenRef.current = _token;
+
+    const _sourceChainOptions = getSourceChainOptions(_token.category);
+    const _sourceChain =
+      _sourceChainOptions.find(({ id }) => id === sourceChainRef.current.id) || _sourceChainOptions[0];
+
+    const _sourceTokenOptions = getSourceTokenOptions(_sourceChain, _token.category);
+    const _sourceToken =
+      _sourceTokenOptions.find(({ symbol }) => symbol === sourceTokenRef.current.symbol) || _sourceTokenOptions[0];
+
+    const _targetChainOptions = getTargetChainOptions(_sourceToken);
+    const _targetChain =
+      _targetChainOptions.find(({ id }) => id === targetChainRef.current.id) || _targetChainOptions[0];
+
+    const _targetTokenOptions = getTargetTokenOptions(_sourceToken, _targetChain);
+    const _targetToken =
+      _targetTokenOptions.find(({ symbol }) => symbol === targetTokenRef.current.symbol) || _targetTokenOptions[0];
+
+    setSourceChain(_sourceChain);
+    setSourceToken(_sourceToken);
+    setTargetChain(_targetChain);
+    setTargetToken(_targetToken);
+
+    sourceChainRef.current = _sourceChain;
+    sourceTokenRef.current = _sourceToken;
+    targetChainRef.current = _targetChain;
+    targetTokenRef.current = _targetToken;
+  }, []);
+
+  const handleSourceChainChange = useCallback((_sourceChain: typeof sourceChain) => {
+    setSourceChain(_sourceChain);
+    sourceChainRef.current = _sourceChain;
+
+    const _sourceTokenOptions = getSourceTokenOptions(_sourceChain, tokenRef.current.category);
+    const _sourceToken =
+      _sourceTokenOptions.find(({ symbol }) => symbol === sourceTokenRef.current.symbol) || _sourceTokenOptions[0];
+
+    const _targetChainOptions = getTargetChainOptions(_sourceToken);
+    const _targetChain =
+      _targetChainOptions.find(({ id }) => id === targetChainRef.current.id) || _targetChainOptions[0];
+
+    const _targetTokenOptions = getTargetTokenOptions(_sourceToken, _targetChain);
+    const _targetToken =
+      _targetTokenOptions.find(({ symbol }) => symbol === targetTokenRef.current.symbol) || _targetTokenOptions[0];
+
+    setSourceToken(_sourceToken);
+    setTargetChain(_targetChain);
+    setTargetToken(_targetToken);
+
+    sourceTokenRef.current = _sourceToken;
+    targetChainRef.current = _targetChain;
+    targetTokenRef.current = _targetToken;
+  }, []);
+
+  const handleSourceTokenChange = useCallback((_sourceToken: typeof sourceToken) => {
+    setSourceToken(_sourceToken);
+    sourceTokenRef.current = _sourceToken;
+
+    const _targetChainOptions = getTargetChainOptions(_sourceToken);
+    const _targetChain =
+      _targetChainOptions.find(({ id }) => id === targetChainRef.current.id) || _targetChainOptions[0];
+
+    const _targetTokenOptions = getTargetTokenOptions(_sourceToken, _targetChain);
+    const _targetToken =
+      _targetTokenOptions.find(({ symbol }) => symbol === targetTokenRef.current.symbol) || _targetTokenOptions[0];
+
+    setTargetChain(_targetChain);
+    setTargetToken(_targetToken);
+
+    targetChainRef.current = _targetChain;
+    targetTokenRef.current = _targetToken;
+  }, []);
+
+  const handleTargetChainChange = useCallback((_targetChain: typeof targetChain) => {
+    setTargetChain(_targetChain);
+    targetChainRef.current = _targetChain;
+
+    const _targetTokenOptions = getTargetTokenOptions(sourceTokenRef.current, _targetChain);
+    const _targetToken =
+      _targetTokenOptions.find(({ symbol }) => symbol === targetTokenRef.current.symbol) || _targetTokenOptions[0];
+
+    setTargetToken(_targetToken);
+    targetTokenRef.current = _targetToken;
+  }, []);
+
+  const handleTargetTokenChange = useCallback((_targetToken: typeof targetToken) => {
+    setTargetToken(_targetToken);
+    targetTokenRef.current = _targetToken;
+  }, []);
+
+  return (
+    <div className="flex w-full flex-col gap-medium rounded-large bg-[#1F282C] p-medium lg:w-[27.5rem] lg:gap-5 lg:rounded-[1.25rem] lg:p-5">
+      <TransferTokenSection token={token} options={tokenOptions} onChange={handleTokenChange} />
+      <TransferChainSection
+        sourceChain={sourceChain}
+        targetChain={targetChain}
+        sourceToken={sourceToken}
+        targetToken={targetToken}
+        sourceChainOptions={getSourceChainOptions(token.category)}
+        targetChainOptions={getTargetChainOptions(sourceToken)}
+        sourceTokenOptions={getSourceTokenOptions(sourceChain, token.category)}
+        targetTokenOptions={getTargetTokenOptions(sourceToken, targetChain)}
+        onSourceChainChange={handleSourceChainChange}
+        onSourceTokenChange={handleSourceTokenChange}
+        onTargetChainChange={handleTargetChainChange}
+        onTargetTokenChange={handleTargetTokenChange}
+      />
+      <TransferAmountSection amount={amount} balance={2024n} token={sourceToken} onChange={setAmount} />
+      <TransferInformationSection />
+    </div>
+  );
+}
