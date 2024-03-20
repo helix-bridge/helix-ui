775d21191a52611b7f84930a20f2fc66a77686a0
JayJay1024 <lygm1062@gmail.com>
2024-03-20 14:46:19 +0800

    refactor: update manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const ibm_plex_sans = IBM_Plex_Sans({
 
 export const metadata: Metadata = {
   title: "Helix Bridge",
-  description: "Helix Bridge App",
+  description: "Helix Bridge Apps",
   manifest: "/manifest.json",
 };
 
