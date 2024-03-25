2553099841ec45ae1762d4b1d7f6a007cc9647c3
JayJay1024 <lygm1062@gmail.com>
2024-03-25 11:36:52 +0800

    feat: token info tooltips

--- src/components/transfer-section-title.tsx
+++ src/components/transfer-section-title.tsx
@@ -11,8 +11,8 @@ export default function TransferSectionTitle({ text, tips }: Props) {
     <div className="inline-flex items-center gap-small">
       <span className="text-sm font-normal text-white/50">{text}</span>
       {tips ? (
-        <Tooltip content={tips}>
-          <Image width={16} height={16} alt="Info" src="/images/info.svg" />
+        <Tooltip content={tips} enabledSafePolygon>
+          <Image width={14} height={14} alt="Info" src="/images/info.svg" />
         </Tooltip>
       ) : null}
     </div>
