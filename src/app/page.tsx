3c6072a7ffe8957a36130e16dc9ab98c746c1d3a
Jay <lygm1062@gmail.com>
2024-04-17 12:57:33 +0800

    Optimize some styles (#696)
    * Update tooltips default font size
    
    * Update token tips font size
    
    * Optimize transfer-v2 styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
         <div className="h-[70vw] w-[70vw] rounded-full bg-primary blur-[8rem] lg:h-[65vh] lg:w-[65vh] lg:bg-primary/40" />
       </div>
       <div className="absolute bottom-0 left-0 right-0 top-0 z-[2] overflow-y-auto">
-        <div className="page-container">
+        <div className="page-container flex min-h-full items-start justify-center lg:items-center lg:!pb-10">
           <TransferV2 />
         </div>
       </div>
