e9c7b64df33e93326f5e91692d685f1ddc615ccf
JayJay1024 <lygm1062@gmail.com>
2024-05-13 15:06:05 +0800

    Setup rainbow

---
+++ src/providers/rainbow-provider.tsx
@@ -0,0 +1,46 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider, connectorsForWallets, getDefaultWallets, darkTheme } from "@rainbow-me/rainbowkit";
+import { okxWallet, rabbyWallet, safeWallet, talismanWallet } from "@rainbow-me/rainbowkit/wallets";
+import { PropsWithChildren } from "react";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "";
+const appName = "Helix Bridge";
+
+const { chains, publicClient } = configureChains([], [publicProvider()]);
+
+const { wallets } = getDefaultWallets({ appName, projectId, chains });
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "More",
+    wallets: [
+      okxWallet({ chains, projectId }),
+      rabbyWallet({ chains }),
+      talismanWallet({ chains }),
+      safeWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function RainbowProvider({ children }: PropsWithChildren<unknown>) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        theme={darkTheme({ borderRadius: "large", accentColor: "#0085FF" })}
+        chains={chains}
+        appInfo={{ appName }}
+      >
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
