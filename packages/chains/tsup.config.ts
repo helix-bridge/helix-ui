8ac56a3b9854590b94d3340c45c91eba944c337c
JayJay1024 <lygm1062@gmail.com>
2024-07-26 13:05:49 +0800

    Update exports

--- packages/chains/tsup.config.ts
+++ packages/chains/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["src/chains/index.ts", "src/types/index.ts", "src/utils/index.ts"],
+  entry: ["src/index.ts", "src/chains/index.ts", "src/types/index.ts", "src/utils/index.ts"],
   clean: true,
   dts: true,
   format: ["cjs", "esm"],
