664202a32458c21f212c1cb71ceb65f6e6f8cfc7
JayJay1024 <lygm1062@gmail.com>
2024-07-26 18:49:06 +0800

    Verify chains configurations

---
+++ packages/chains/src/utils/index.test.ts
@@ -0,0 +1,30 @@
+import { HelixChain } from "@helixbridge/helixconf";
+import { getChainByIdOrNetwork, getChains } from ".";
+import { ChainID } from "../types";
+
+const numberOfChains = Object.keys(ChainID).length / 2;
+const chains = getChains();
+
+test(`The number of chains should be equal to ${numberOfChains}`, () => {
+  expect(chains.length).toBe(numberOfChains);
+});
+
+describe.each(getChains())("$name", ({ id, network }) => {
+  if (network === "taiko" || network === "pangolin-dvm") {
+    /**
+     * @helixbridge/helixconf does not support these chains.
+     * Maybe we should remove these chains from @helixbridge/chains too.
+     */
+  } else {
+    test(`The 'network' should be configured correctly`, () => {
+      expect(network).toBe(HelixChain.get(id)?.code);
+    });
+  }
+
+  test(`getChainByIdOrNetwork(${id}) should return the correct chain`, () => {
+    expect(getChainByIdOrNetwork(id)?.network).toBe(network);
+  });
+  test(`getChainByIdOrNetwork('${network}') should return the correct chain`, () => {
+    expect(getChainByIdOrNetwork(network)?.id).toBe(id);
+  });
+});
