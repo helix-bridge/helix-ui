f6223881d6459c593a051a9c95c35bc8b632f8f1
JayJay1024 <lygm1062@gmail.com>
2024-08-06 19:21:08 +0800

    Fix withdraw liquidities load more

--- apps/web/src/hooks/use-withdrawable-liquidities.ts
+++ apps/web/src/hooks/use-withdrawable-liquidities.ts
@@ -1,7 +1,7 @@
 import { GQL_GET_WITHDRAWABLE_LIQUIDITIES } from "../config";
 import { Network, WithdrawableLiquiditiesReqParams, WithdrawableLiquiditiesResData } from "../types";
 import { useQuery } from "@apollo/client";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { Address } from "viem";
 
 export function useWithdrawableLiquidities(
@@ -10,22 +10,29 @@ export function useWithdrawableLiquidities(
   fromChain: Network | null | undefined,
   toChain: Network | null | undefined,
 ) {
+  const pageRef = useRef(0);
+  useEffect(() => {
+    pageRef.current = 0;
+  }, [relayer, recvTokenAddress, fromChain, toChain]);
+
   const { loading, data, refetch, fetchMore } = useQuery<
     WithdrawableLiquiditiesResData,
     WithdrawableLiquiditiesReqParams
   >(GQL_GET_WITHDRAWABLE_LIQUIDITIES, {
     variables: { page: 0, relayer, recvTokenAddress, fromChain, toChain },
     skip: !relayer || !recvTokenAddress || !fromChain || !toChain,
-    fetchPolicy: "no-cache",
+    // fetchPolicy: "no-cache",  // Should use cache to adapt fetchMore
   });
 
   const handleRefetch = useCallback(() => {
     refetch({ page: 0 });
+    pageRef.current = 0;
   }, [refetch]);
 
   const handleFetchMore = useCallback(() => {
-    fetchMore({ variables: { page: data?.historyRecords.records.length } });
-  }, [data?.historyRecords.records.length, fetchMore]);
+    pageRef.current = pageRef.current + 1;
+    fetchMore({ variables: { page: pageRef.current } });
+  }, [fetchMore]);
 
   return {
     loading,
