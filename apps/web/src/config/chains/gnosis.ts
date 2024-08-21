0cbfc855a808a30830eb117114fc2ac66ecbb762
JayJay1024 <lygm1062@gmail.com>
2024-08-21 14:28:23 +0800

    Fix explorer of Gnosis

--- apps/web/src/config/chains/gnosis.ts
+++ apps/web/src/config/chains/gnosis.ts
@@ -54,6 +54,12 @@ export const gnosisChain: ChainConfig = {
   ...gnosis,
   network: "gnosis",
   name: "Gnosis Chain",
+  blockExplorers: {
+    default: {
+      name: "Gnosisscan",
+      url: "https://gnosisscan.io",
+    },
+  },
 
   /**
    * Custom
