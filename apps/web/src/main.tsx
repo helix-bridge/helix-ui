62156db74c97896e0dbfa5d949d51d850ac3fbaf
JayJay1024 <lygm1062@gmail.com>
2024-08-29 11:13:08 +0800

    Add PWABadge

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
+import PWABadge from "./components/pwa-badge.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <PWABadge />
   </React.StrictMode>,
 );
