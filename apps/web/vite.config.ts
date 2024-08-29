4e3c1406955d65453043bd90955c7a720f388f28
Jay <lygm1062@gmail.com>
2024-08-29 11:29:19 +0800

    Support PWA (#806)
    * Beta
    
    * Use plugin to generate pwa assets
    
    * Add PWABadge

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,9 +2,38 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+      manifestFilename: "manifest.json",
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      manifest: {
+        name: "Helix Bridge",
+        short_name: "HelixBridge",
+        description: "Secure, fast, and low-cost cross-chain crypto transfers",
+        theme_color: "#00141D",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
 });
