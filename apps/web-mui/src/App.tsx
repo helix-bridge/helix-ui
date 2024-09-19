50b2da9980879de9623d082187adf064c8d826d9
JayJay1024 <lygm1062@gmail.com>
2024-09-19 16:15:58 +0800

    Theme demo

--- apps/web-mui/src/App.tsx
+++ apps/web-mui/src/App.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@mui/material";
+import { Button, Typography, useColorScheme } from "@mui/material";
 
 function App() {
+  const { mode, setMode } = useColorScheme();
+
   return (
     <>
-      <Button variant="contained">Hello World</Button>
+      <Typography variant="body1">Mode: {mode}</Typography>
+      <Button variant="outlined" onClick={() => setMode("dark")}>
+        Dark Mode
+      </Button>
+      <Button variant="outlined" onClick={() => setMode("light")}>
+        Light Mode
+      </Button>
+      <Button variant="contained" onClick={() => setMode("system")}>
+        System Mode
+      </Button>
     </>
   );
 }
--- apps/web-mui/src/main.tsx
+++ apps/web-mui/src/main.tsx
@@ -7,11 +7,17 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  colorSchemes: { dark: true },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
