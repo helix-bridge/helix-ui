093c716fcc8889547f36cfcaff8da8a2c7a5cc7e
JayJay1024 <lygm1062@gmail.com>
2024-09-19 15:47:02 +0800

    Setup MUI

--- apps/web-mui/src/App.tsx
+++ apps/web-mui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,6 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Button variant="contained">Hello World</Button>
     </>
   );
 }
--- apps/web-mui/src/main.tsx
+++ apps/web-mui/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
