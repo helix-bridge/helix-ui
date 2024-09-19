887e900c5206cd901dd5cc68772be2a9e4f41c48
JayJay1024 <lygm1062@gmail.com>
2024-09-19 15:58:29 +0800

    Setup CssBaseline

--- apps/web-mui/src/main.tsx
+++ apps/web-mui/src/main.tsx
@@ -7,9 +7,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 );
