966e8454e813dac1ee6d38175634a06f0b5c06b1
JayJay1024 <lygm1062@gmail.com>
2024-09-25 12:29:06 +0800

    Test

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -19,7 +19,7 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
-console.log("Hello 1");
+console.log("Hello 2");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
