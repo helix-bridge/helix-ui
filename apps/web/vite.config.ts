eb57ba896b699e348c1a102227f6cfc9dab89a8a
JayJay1024 <lygm1062@gmail.com>
2024-10-17 09:40:33 +0800

    Unregister the service worker

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         suppressWarnings: true,
         type: "module",
       },
+      selfDestroying: true,
     }),
     sentryVitePlugin({
       org: "helix-ck",
