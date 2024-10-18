8ba304f8e1b030a68e65b56251a2343bb626542c
JayJay1024 <lygm1062@gmail.com>
2024-10-18 12:31:17 +0800

    Update sorted relayers data after loading is completed

--- apps/web/src/hooks/use-sorted-relayers.ts
+++ apps/web/src/hooks/use-sorted-relayers.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { graphql } from "../_generated_/gql";
 import { ChainConfig, Token } from "../types";
 import { useQuery } from "@apollo/client";
@@ -36,7 +37,11 @@ const document = graphql(`
 `);
 
 export function useSortedRelayers(amount: bigint, token: Token, sourceChain: ChainConfig, targetChain: ChainConfig) {
-  const { loading, data, refetch } = useQuery(document, {
+  const {
+    loading,
+    data: _data,
+    refetch,
+  } = useQuery(document, {
     variables: {
       amount: amount.toString(),
       decimals: token.decimals,
@@ -46,6 +51,13 @@ export function useSortedRelayers(amount: bigint, token: Token, sourceChain: Cha
     },
     fetchPolicy: "cache-and-network",
   });
+  const [data, setData] = useState(_data);
+
+  useEffect(() => {
+    if (!loading) {
+      setData(_data);
+    }
+  }, [loading, _data]);
 
   return { loading, data, refetch };
 }
