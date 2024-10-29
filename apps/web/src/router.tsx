79d497d45b05504b7c1bbe4e59cf2a4fa9c66a80
JayJay1024 <lygm1062@gmail.com>
2024-10-29 16:54:53 +0800

    Fix layout

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -6,16 +6,24 @@ import Error from "./routes/error";
 import Explorer from "./routes/explorer";
 import TxDetails from "./routes/tx-details";
 import Home from "./routes/home";
-import Layout from "./layout";
 import Solver from "./routes/solver";
+import HomeLayout from "./layout/home-layout";
+import AppLayout from "./layout/app-layout";
 
 export const router = createHashRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <HomeLayout />,
     children: [
       { index: true, element: <Home /> },
       { path: "liquidity-solver", element: <Solver /> },
+    ],
+    errorElement: <Error />,
+  },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
       { path: "transfer", element: <Transfer /> },
       { path: "relayer", element: <Relayer /> },
       { path: "explorer", element: <Explorer /> },
