a846acfa092359df54d1a426abcfa2ea62233130
JayJay1024 <lygm1062@gmail.com>
2024-11-01 14:16:11 +0800

    Update products scale

--- apps/helixbox-home/src/components/homepage-products/index.tsx
+++ apps/helixbox-home/src/components/homepage-products/index.tsx
@@ -16,7 +16,7 @@ export default function HomepageProducts() {
           <Mobile key={product.title} {...product} />
         ))}
       </div>
-      <div className="relative hidden scale-75 items-center justify-center lg:flex 2xl:scale-100">
+      <div className="relative hidden origin-center scale-75 items-center justify-center lg:flex 2xl:scale-100">
         <AnimatedVideo video={video} />
 
         <div className="absolute -top-[94px] left-[50%] -translate-x-[50%]">
