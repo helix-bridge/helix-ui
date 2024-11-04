a0d5c3791178aff9da61d350e228c86e8e4a1066
fewensa <37804932+fewensa@users.noreply.github.com>
2024-11-04 14:54:21 +0800

    Update helixbridge.app to helix.box

--- apps/helixbox-app/codegen.ts
+++ apps/helixbox-app/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://apollo.helixbridge.app/graphql",
+  schema: "https://apollo.helix.box/graphql",
   documents: ["src/hooks/**/*.ts", "src/components/**/*.tsx"],
   generates: {
     "src/_generated_/gql/": {
