660689d705b4974fb8d802252afb893727b1a271
JayJay1024 <lygm1062@gmail.com>
2024-12-06 12:17:51 +0800

    Jotai atom use case

--- apps/smart-wallet/src/pages/Home.tsx
+++ apps/smart-wallet/src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Header } from "../components/Header";
 import { ToggleThemeButton } from "../components/ToggleThemeButton";
+import { countAtom } from "../atoms";
+import { useAtom } from "jotai";
+
 export default function Home() {
+  const [count, setCount] = useAtom(countAtom);
+
   return (
     <Box>
       <Header />
       <ToggleThemeButton />
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
+      <p>Count: {count}</p>
     </Box>
   );
 }
