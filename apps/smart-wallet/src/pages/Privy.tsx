90463f786e48c8c1dd2c15f281bd06cc98fdf075
JayJay1024 <lygm1062@gmail.com>
2024-12-09 14:26:23 +0800

    Logout

--- apps/smart-wallet/src/pages/Privy.tsx
+++ apps/smart-wallet/src/pages/Privy.tsx
@@ -3,7 +3,7 @@ import { useFundWallet, usePrivy } from "@privy-io/react-auth";
 import { base } from "viem/chains";
 
 export default function Privy() {
-  const { ready, user, authenticated, login } = usePrivy();
+  const { ready, user, authenticated, login, logout } = usePrivy();
   const { fundWallet } = useFundWallet();
 
   return (
@@ -18,23 +18,36 @@ export default function Privy() {
         alignItems: "center",
       }}
     >
-      <Button disabled={!ready || authenticated} onClick={login} variant="contained">
-        Log in or sign up
-      </Button>
-      <Button
-        onClick={() =>
-          fundWallet(user?.wallet?.address ?? "0xf422673cb7a673f595852f7b00906408a0b073db", {
-            defaultFundingMethod: "manual",
-            chain: base,
-            amount: "123",
-            asset: { erc20: "0x0Db510e79909666d6dEc7f5e49370838c16D950f" },
-          })
-        }
-        disabled={!ready || !authenticated}
-        variant="contained"
-      >
-        Fund
-      </Button>
+      {!authenticated && (
+        <Button disabled={!ready} onClick={login} variant="contained">
+          Log in or sign up
+        </Button>
+      )}
+
+      {authenticated && (
+        <Button disabled={!ready} onClick={logout} variant="contained">
+          Logout
+        </Button>
+      )}
+
+      {authenticated && (
+        <Button
+          onClick={() =>
+            fundWallet(user?.wallet?.address ?? "0xf422673cb7a673f595852f7b00906408a0b073db", {
+              defaultFundingMethod: "manual",
+              // chain: base,
+              // amount: "123",
+              // asset: { erc20: "0x0Db510e79909666d6dEc7f5e49370838c16D950f" },
+            })
+          }
+          disabled={!ready}
+          variant="contained"
+        >
+          Fund
+        </Button>
+      )}
+
+      {user ? <pre>{JSON.stringify(user, null, 2)}</pre> : null}
     </Box>
   );
 }
