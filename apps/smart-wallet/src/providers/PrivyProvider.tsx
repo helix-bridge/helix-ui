db041a90c331f1b9c8c3e47778c90dc703406013
JayJay1024 <lygm1062@gmail.com>
2024-12-12 11:03:52 +0800

    Setup smart wallet

--- apps/smart-wallet/src/providers/PrivyProvider.tsx
+++ apps/smart-wallet/src/providers/PrivyProvider.tsx
@@ -1,12 +1,11 @@
 import { PrivyProvider as PrivyProviderFromPrivy } from "@privy-io/react-auth";
-// import { SmartWalletsProvider } from "@privy-io/react-auth/smart-wallets";
+import { SmartWalletsProvider } from "@privy-io/react-auth/smart-wallets";
 import { PropsWithChildren } from "react";
 
 export default function PrivyProvider({ children }: PropsWithChildren<unknown>) {
   return (
     <PrivyProviderFromPrivy appId="cm4c8vv3y0846119zl29qwi9t">
-      {/* <SmartWalletsProvider></SmartWalletsProvider> */}
-      {children}
+      <SmartWalletsProvider>{children}</SmartWalletsProvider>
     </PrivyProviderFromPrivy>
   );
 }
